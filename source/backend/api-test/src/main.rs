da54dff7b22477a2762ab43c598dc43b4269c983
takashi605 <95337577+takashi605@users.noreply.github.com>
2024-09-07 14:47:50 +0900

    エラーハンドリングに anyhow を使うようにした #57

--- source/backend/api-test/src/main.rs
+++ source/backend/api-test/src/main.rs
@@ -8,20 +8,22 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
 #[cfg(test)]
 mod tests {
+  use anyhow::{Context, Result};
+
   #[tokio::test(flavor = "current_thread")]
-  async fn root_get() -> Result<(), Box<dyn std::error::Error>> {
+  async fn root_get() -> Result<()> {
     let resp = reqwest::get("http://localhost:8000")
-        .await?.text().await?;
+        .await.context("/ に対する get リクエストを正常に送信できませんでした")?.text().await.context("/ に対して get リクエストを送信しましたが、レスポンスをテキストに変換できませんでした")?;
     assert_eq!(resp,"Hello world!");
     Ok(())
   }
 
   #[tokio::test(flavor = "current_thread")]
-  async fn root_post() -> Result<(), Box<dyn std::error::Error>> {
+  async fn root_post() -> Result<()> {
     let resp: String = reqwest::Client::new()
         .post("http://localhost:8000")
         .body("post message")
-        .send().await?.text().await?;
+        .send().await.context("/ に対する post リクエストを正常に送信できませんでした")?.text().await.context("/ に対して post リクエストを送信しましたが、レスポンスをテキストに変換できませんでした")?;
     assert_eq!(resp,"post message");
     Ok(())
   }
