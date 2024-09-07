22c97e312b742c4cb5d66bd8c79cd2b46e06ab4a
takashi605 <95337577+takashi605@users.noreply.github.com>
2024-09-07 12:39:42 +0900

    ルート uri に対する get のテストを書いた #57

--- source/backend/api-test/src/main.rs
+++ source/backend/api-test/src/main.rs
@@ -1,9 +1,18 @@
-use std::collections::HashMap;
-
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let resp = reqwest::get("http://localhost:8000")
         .await?.text().await?;
     println!("{resp:#?}");
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+  #[tokio::test(flavor = "current_thread")]
+  async fn root_get() -> Result<(), Box<dyn std::error::Error>> {
+    let resp = reqwest::get("http://localhost:8000")
+        .await?.text().await?;
+    assert_eq!(resp,"Hello world!");
+    Ok(())
+  }
 }
