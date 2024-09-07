63f115bb90139b1d5be5c289c86d3698dfdbf57e
takashi605 <95337577+takashi605@users.noreply.github.com>
2024-09-07 14:54:03 +0900

    main 関数内の通信処理を削除し、テスト用クレートである旨を print するようにした #57

--- source/backend/api-test/src/main.rs
+++ source/backend/api-test/src/main.rs
@@ -1,8 +1,5 @@
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let resp = reqwest::get("http://localhost:8000").await?.text().await?;
-    println!("{resp:#?}");
-    Ok(())
+fn main() {
+    println!("これは API テスト用のクレートです。cargo test コマンドでテストを実行してください。");
 }
 
 #[cfg(test)]
@@ -12,7 +9,7 @@ mod tests {
     #[tokio::test(flavor = "current_thread")]
     async fn root_get() -> Result<()> {
         let resp = reqwest::get("http://localhost:8000")
-        .await.context("/ に対する get リクエストを正常に送信できませんでした")?.text().await.context("/ に対して get リクエストを送信しましたが、レスポンスをテキストに変換できませんでした")?;
+      .await.context("/ に対する get リクエストを正常に送信できませんでした")?.text().await.context("/ に対して get リクエストを送信しましたが、レスポンスをテキストに変換できませんでした")?;
         assert_eq!(resp, "Hello world!");
         Ok(())
     }
@@ -20,10 +17,10 @@ mod tests {
     #[tokio::test(flavor = "current_thread")]
     async fn root_post() -> Result<()> {
         let resp: String = reqwest::Client::new()
-        .post("http://localhost:8000")
-        .body("post message")
-        .send().await.context("/ に対する post リクエストを正常に送信できませんでした")?
-        .text().await.context("/ に対して post リクエストを送信しましたが、レスポンスをテキストに変換できませんでした")?;
+      .post("http://localhost:8000")
+      .body("post message")
+      .send().await.context("/ に対する post リクエストを正常に送信できませんでした")?
+      .text().await.context("/ に対して post リクエストを送信しましたが、レスポンスをテキストに変換できませんでした")?;
         assert_eq!(resp, "post message");
         Ok(())
     }
