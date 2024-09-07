6c9e25d1ecf6090f6510f572bf9997176e195dbd
takashi605 <95337577+takashi605@users.noreply.github.com>
2024-09-07 14:49:15 +0900

    保存時に rust のフォーマッタが走るようにした #57

--- source/backend/api-test/src/main.rs
+++ source/backend/api-test/src/main.rs
@@ -1,30 +1,30 @@
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let resp = reqwest::get("http://localhost:8000")
-        .await?.text().await?;
+    let resp = reqwest::get("http://localhost:8000").await?.text().await?;
     println!("{resp:#?}");
     Ok(())
 }
 
 #[cfg(test)]
 mod tests {
-  use anyhow::{Context, Result};
+    use anyhow::{Context, Result};
 
-  #[tokio::test(flavor = "current_thread")]
-  async fn root_get() -> Result<()> {
-    let resp = reqwest::get("http://localhost:8000")
+    #[tokio::test(flavor = "current_thread")]
+    async fn root_get() -> Result<()> {
+        let resp = reqwest::get("http://localhost:8000")
         .await.context("/ に対する get リクエストを正常に送信できませんでした")?.text().await.context("/ に対して get リクエストを送信しましたが、レスポンスをテキストに変換できませんでした")?;
-    assert_eq!(resp,"Hello world!");
-    Ok(())
-  }
+        assert_eq!(resp, "Hello world!");
+        Ok(())
+    }
 
-  #[tokio::test(flavor = "current_thread")]
-  async fn root_post() -> Result<()> {
-    let resp: String = reqwest::Client::new()
+    #[tokio::test(flavor = "current_thread")]
+    async fn root_post() -> Result<()> {
+        let resp: String = reqwest::Client::new()
         .post("http://localhost:8000")
         .body("post message")
-        .send().await.context("/ に対する post リクエストを正常に送信できませんでした")?.text().await.context("/ に対して post リクエストを送信しましたが、レスポンスをテキストに変換できませんでした")?;
-    assert_eq!(resp,"post message");
-    Ok(())
-  }
+        .send().await.context("/ に対する post リクエストを正常に送信できませんでした")?
+        .text().await.context("/ に対して post リクエストを送信しましたが、レスポンスをテキストに変換できませんでした")?;
+        assert_eq!(resp, "post message");
+        Ok(())
+    }
 }
