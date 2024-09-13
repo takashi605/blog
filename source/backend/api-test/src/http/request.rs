fb61131091452bc4236676d3e07b2bd9712ad6f5
takashi605 <95337577+takashi605@users.noreply.github.com>
2024-09-14 08:08:39 +0900

    RequestBuilder を構造体に持たせるようにして、リクエストの情報をセットする処理を送信処理からコンストラクタに移動した #63

--- source/backend/api-test/src/http/request.rs
+++ source/backend/api-test/src/http/request.rs
@@ -4,26 +4,27 @@ use crate::http::methods::Methods;
 use crate::http::response::Response;
 
 pub struct Request {
+  request_builder: reqwest::RequestBuilder,
   pub method: Methods,
   url: String,
-  _body: Option<String>,
   _headers: Option<Vec<(String, String)>>,
   _url_params: Option<Vec<(String, String)>>,
 }
 
 impl Request {
   pub fn new(method: Methods, url: &str) -> Self {
-    Request { method, url: url.to_string(), _body: None, _headers: None, _url_params: None }
+    let client = reqwest::Client::new();
+    let request_builder = match &method {
+      Methods::GET => client.get(url),
+      Methods::POST { body } => client.post(url).body(body.to_string()),
+      Methods::PUT => client.put(url),
+      Methods::DELETE => client.delete(url),
+    };
+    Request { request_builder, method, url: url.to_string(), _headers: None, _url_params: None }
   }
 
-  pub async fn send(&self) -> Result<Response> {
-    let client = reqwest::Client::new();
-    let resp = match &self.method {
-      Methods::GET => client.get(&self.url).send().await.context("get リクエストを正常に送信できませんでした")?,
-      Methods::POST { body } => client.post(&self.url).body(body.to_string()).send().await.context("post リクエストを正常に送信できませんでした")?,
-      Methods::PUT => client.put(&self.url).send().await.context("put リクエストを正常に送信できませんでした")?,
-      Methods::DELETE => client.delete(&self.url).send().await.context("delete リクエストを正常に送信できませんでした")?,
-    };
+  pub async fn send(self) -> Result<Response> {
+    let resp = self.request_builder.send().await.context("リクエストを送信できませんでした")?;
     Ok(Response::new(resp))
   }
 }
