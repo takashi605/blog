df0920d43fd5cb2bca12f2a87de186d90439b7fe
takashi605 <95337577+takashi605@users.noreply.github.com>
2024-09-14 09:02:47 +0900

    /fivesix の GET エンドポイントを作成して api テストを通した #63

--- source/backend/api/src/main.rs
+++ source/backend/api/src/main.rs
@@ -10,6 +10,17 @@ async fn echo(req_body: String) -> impl Responder {
   HttpResponse::Ok().body(req_body)
 }
 
+#[get("/fivesix")]
+async fn fivesix() -> impl Responder {
+  #[derive(serde::Serialize)]
+  struct Numbers {
+    num1: i32,
+    num2: i32,
+  }
+  let numbers = Numbers { num1: 5, num2: 6 };
+  HttpResponse::Ok().json(numbers)
+}
+
 async fn manual_hello() -> impl Responder {
   HttpResponse::Ok().body("Hey there!")
 }
@@ -17,5 +28,6 @@ async fn manual_hello() -> impl Responder {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
   println!("api started");
-  HttpServer::new(|| App::new().service(hello).service(echo).route("/hey", web::get().to(manual_hello))).bind(("127.0.0.1", 8000))?.run().await
+  HttpServer::new(|| App::new().service(hello).service(echo).service(fivesix).route("/hey", web::get().to(manual_hello))).bind(("127.0.0.1", 8000))?.run().await
 }
+
