0f784ea504dd59fa325df8939dad63c5288ccc2d
takashi605 <95337577+takashi605@users.noreply.github.com>
2024-09-15 10:56:16 +0900

    落ちていたユニットテストを修正した #63

--- source/frontend/packages/web/src/testMocks/handlers.tsx
+++ source/frontend/packages/web/src/testMocks/handlers.tsx
@@ -1,7 +1,7 @@
 import { http, HttpResponse } from 'msw';
 
 export const handlers = [
-  http.get('https://api/fivesix', () => {
+  http.get('http://192.168.1.1/api/fivesix', () => {
     return HttpResponse.json({
       num1: 5,
       num2: 6,
