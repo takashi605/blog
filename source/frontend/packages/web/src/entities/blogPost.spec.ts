de3747c90aec6eed533401aa613be70febc2c500
takashi605 <95337577+takashi605@users.noreply.github.com>
2024-10-05 19:07:40 +0900

    既存のエンティティのテストの名前を現状に即したものに変更 #84

--- source/frontend/packages/web/src/entities/blogPost.spec.ts
+++ source/frontend/packages/web/src/entities/blogPost.spec.ts
@@ -1,7 +1,7 @@
 import { createBlogPost, type BlogPost } from './blogPost';
 
 describe('エンティティ: 投稿記事', () => {
-  it('ブログ記事の構造を生成できる', async () => {
+  it('記事タイトルとなる h1 見出しを生成できる', async () => {
     const title = '記事タイトル'
     const blogPost: BlogPost = createBlogPost(title);
     expect(blogPost.getTitleText()).toBe(title);
