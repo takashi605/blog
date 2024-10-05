40d57f538bbe0712fe2cf453e7dc7e3b786f2c3e
takashi605 <95337577+takashi605@users.noreply.github.com>
2024-10-05 16:53:55 +0900

    ユースケース層のテストを通しつつ、input,output をより適切な形に変更 #84

--- source/frontend/packages/web/src/usecases/viewBlogPost.spec.ts
+++ source/frontend/packages/web/src/usecases/viewBlogPost.spec.ts
@@ -4,12 +4,9 @@ import { viewBlogPost } from './viewBlogPost';
 describe('ユースケース: 投稿記事の閲覧', () => {
   it('記事のタイトルを入力値として受け取り、ブログ記事の構造として返却する', async () => {
     const input: ViewBlogPostInput = {
-      postTitle: {
-        text: '記事タイトル',
-        level: 1,
-      },
+      postTitle: '記事タイトル',
     };
     const output: ViewBlogPostOutput = viewBlogPost(input);
-    expect(output.postTitle).toBe('記事タイトル');
+    expect(output.postTitle.getTitle()).toBe('記事タイトル');
   });
 });
--- source/frontend/packages/web/src/usecases/viewBlogPost.ts
+++ source/frontend/packages/web/src/usecases/viewBlogPost.ts
@@ -1,17 +1,21 @@
 import { createBlogPost } from "@/entities/blogPost";
-import type { BlogPost, Heading } from "@/entities/blogPost";
+import type { BlogPost } from "@/entities/blogPost";
 
 export type ViewBlogPostInput = {
-  postTitle: Heading;
+  postTitle: string;
 };
 
 export type ViewBlogPostOutput = {
-  postTitle: string;
+  postTitle: {
+    getTitle: () => string;
+  };
 };
 
 export const viewBlogPost = (input: ViewBlogPostInput): ViewBlogPostOutput => {
   const blogPost: BlogPost = createBlogPost(input.postTitle);
   return {
-    postTitle: blogPost.getTitle(),
+    postTitle: {
+      getTitle: () => blogPost.getTitle(),
+    },
   };
 };
