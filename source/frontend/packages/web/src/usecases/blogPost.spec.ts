3a4e4bafe8c161f7f2939d2d5a23ded2637f92ae
takashi605 <95337577+takashi605@users.noreply.github.com>
2024-10-05 14:22:52 +0900

    usecases/blogPost.spec.ts の describe 名を変更 #84

--- source/frontend/packages/web/src/usecases/blogPost.spec.ts
+++ source/frontend/packages/web/src/usecases/blogPost.spec.ts
@@ -1,7 +1,7 @@
 import type { Input, Output } from './blogPost';
 import { createBlogPost } from './blogPost';
 
-describe('投稿記事の閲覧', () => {
+describe('ユースケース: 投稿記事', () => {
   it('記事のタイトルを入力値として受け取り、記事の構造を生成できる', async () => {
     const input: Input = {
       postTitle: '記事タイトル',
