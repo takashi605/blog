9dffada9d3f16961cc56651b2964188ebbc889de
takashi605 <95337577+takashi605@users.noreply.github.com>
2024-10-05 14:15:01 +0900

    投稿記事を生成することができるテストを書き、通した #84

--- source/frontend/packages/web/src/usecases/view.ts
+++ source/frontend/packages/web/src/usecases/view.ts
@@ -1,7 +1,13 @@
-type Input = {
+export type Input = {
   blogTitle: string;
 }
 
-type Output = {
+export type Output = {
   blogTitle: string;
+};
+
+export const createBlogPost = (input: Input): Output => {
+  return {
+    blogTitle: input.blogTitle,
+  }
 }
