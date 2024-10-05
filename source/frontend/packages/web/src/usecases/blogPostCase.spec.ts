3012945ea4566e515963c0c19e2254276e6cb7f8
takashi605 <95337577+takashi605@users.noreply.github.com>
2024-10-05 14:24:03 +0900

    rename usecases/blogPost -> usecases/blogPostCase #84

--- source/frontend/packages/web/src/usecases/blogPostCase.spec.ts
+++ source/frontend/packages/web/src/usecases/blogPostCase.spec.ts
@@ -1,5 +1,5 @@
-import type { Input, Output } from './blogPost';
-import { createBlogPost } from './blogPost';
+import type { Input, Output } from './blogPostCase';
+import { createBlogPost } from './blogPostCase';
 
 describe('ユースケース: 投稿記事', () => {
   it('記事のタイトルを入力値として受け取り、記事の構造を生成できる', async () => {
