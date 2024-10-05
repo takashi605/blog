93b72962d366fc12602cc58763b06fa7d2ff3862
takashi605 <95337577+takashi605@users.noreply.github.com>
2024-10-05 16:47:31 +0900

    blogPost エンティティで createH1 を使うようにした #84

--- source/frontend/packages/web/src/entities/blogPost.spec.ts
+++ source/frontend/packages/web/src/entities/blogPost.spec.ts
@@ -2,11 +2,8 @@ import { createBlogPost, type BlogPost } from './blogPost';
 
 describe('エンティティ: 投稿記事', () => {
   it('ブログ記事の構造を生成できる', async () => {
-    const title = {
-      text: '記事タイトル',
-      level: 1,
-    };
+    const title = '記事タイトル'
     const blogPost: BlogPost = createBlogPost(title);
-    expect(blogPost.getTitle()).toBe(title.text);
+    expect(blogPost.getTitle()).toBe(title);
   });
 });
--- source/frontend/packages/web/src/entities/blogPost.ts
+++ source/frontend/packages/web/src/entities/blogPost.ts
@@ -1,17 +1,14 @@
-export type Heading = {
-  text: string;
-  level: number;
-};
+import { createH1, type Heading } from "./postContants/heading";
 
 export type BlogPost = {
   getTitle: () => string;
 };
 
-export const createBlogPost = (initialTitle: Heading): BlogPost => {
+export const createBlogPost = (initialTitle: string): BlogPost => {
   // setter 等が必要になった時のために title を変数に保持しておく
-  const title = initialTitle;
+  const title = createH1(initialTitle);
 
   return {
-    getTitle: () => title.text,
+    getTitle: () => title.getText(),
   };
 };
