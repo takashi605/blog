7269a11e54de8129bba02c42cd706b46a7f115bb
takashi605 <95337577+takashi605@users.noreply.github.com>
2024-10-06 16:20:04 +0900

    不要となった項目をユースケース内のオブジェクトから削除 #84

--- source/frontend/packages/web/src/usecases/viewBlogPost.ts
+++ source/frontend/packages/web/src/usecases/viewBlogPost.ts
@@ -3,9 +3,6 @@ import { createBlogPost } from '@/entities/blogPost';
 
 export type ViewBlogPostInput = {
   postTitle: string;
-  h2List: string[];
-  h3List: string[];
-  paragraphList: string[];
   contents: {
     type: string;
     contentValue: string;
@@ -17,17 +14,6 @@ export type ViewBlogPostOutput = {
     getText: () => string;
     getLevel: () => number;
   };
-  getH2List: () => {
-    getText: () => string;
-    getLevel: () => number;
-  }[];
-  getH3List: () => {
-    getText: () => string;
-    getLevel: () => number;
-  }[];
-  getParagraphList: () => {
-    getText: () => string;
-  }[];
   getContents: () => {
     getId: () => number;
     getText: () => string;
@@ -38,7 +24,7 @@ export type ViewBlogPostOutput = {
 export const viewBlogPost = (
   input: ViewBlogPostInput
 ): ViewBlogPostOutput => {
-  const blogPost: BlogPost = createBlogPost(input.postTitle, input.h2List, input.h3List, input.paragraphList);
+  const blogPost: BlogPost = createBlogPost(input.postTitle);
 
   input.contents.forEach((content) => {
     if (content.type === 'h2') {
@@ -54,29 +40,6 @@ export const viewBlogPost = (
     postTitle: {
       getText: () => blogPost.getTitleText(),
       getLevel: () => blogPost.getTitleLevel(),
-    },
-    getH2List: () => {
-      return blogPost.getH2List().map((h2) => {
-        return {
-          getText: () => h2.getContent(),
-          getLevel: () => h2.getLevel(),
-        };
-      });
-    },
-    getH3List: () => {
-      return blogPost.getH3List().map((h3) => {
-        return {
-          getText: () => h3.getContent(),
-          getLevel: () => h3.getLevel(),
-        };
-      });
-    },
-    getParagraphList: () => {
-      return blogPost.getParagraphList().map((paragraph) => {
-        return {
-          getText: () => paragraph.getContent(),
-        };
-      });
     },
     getContents: () => {
       return blogPost.getContents().map((content) => {
