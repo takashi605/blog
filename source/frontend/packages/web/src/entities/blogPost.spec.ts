c937c7edd5f80b7da8a709b1660d2904bb61e357
takashi605 <95337577+takashi605@users.noreply.github.com>
2024-10-06 16:22:49 +0900

    エンティティ内のオブジェクトから不要となった項目を削除した #84

--- source/frontend/packages/web/src/entities/blogPost.spec.ts
+++ source/frontend/packages/web/src/entities/blogPost.spec.ts
@@ -1,45 +1,16 @@
 import { createBlogPost, type BlogPost } from './blogPost';
 
 describe('エンティティ: 投稿記事', () => {
-  it('記事タイトルとなる h1 見出しを生成できる', () => {
+  it('記事タイトルを生成できる', () => {
     const title = '記事タイトル';
-    const blogPost: BlogPost = createBlogPost(title, [], []);
+    const blogPost: BlogPost = createBlogPost(title);
     expect(blogPost.getTitleText()).toBe(title);
     expect(blogPost.getTitleLevel()).toBe(1);
   });
 
-  it('h2 見出しを複数生成できる', () => {
+  it('コンテンツとして h2,h3 及び段落を持つ記事を生成できる', () => {
     const title = '記事タイトル';
-    const h2List = ['h2見出し1', 'h2見出し2', 'h2見出し3'];
-    const blogPost: BlogPost = createBlogPost(title, h2List, []);
-    blogPost.getH2List().forEach((h2, index) => {
-      expect(h2.getContent()).toBe(h2List[index]);
-      expect(h2.getLevel()).toBe(2);
-    });
-  });
-
-  it('h3 見出しを複数生成できる', () => {
-    const title = '記事タイトル';
-    const h3List = ['h3見出し1', 'h3見出し2', 'h3見出し3'];
-    const blogPost: BlogPost = createBlogPost(title, [], h3List);
-    blogPost.getH3List().forEach((h3, index) => {
-      expect(h3.getContent()).toBe(h3List[index]);
-      expect(h3.getLevel()).toBe(3);
-    });
-  });
-
-  it('段落を生成できる', () => {
-    const title = '記事タイトル';
-    const paragraphList = ['段落1', '段落2', '段落3'];
-    const blogPost: BlogPost = createBlogPost(title, [], [], paragraphList);
-    blogPost.getParagraphList().forEach((paragraph, index) => {
-      expect(paragraph.getContent()).toBe(paragraphList[index]);
-    });
-  });
-
-  it('コンテンツとしてh2,h3見出し及び段落を持つ記事を生成できる', () => {
-    const title = '記事タイトル';
-    const blogPost: BlogPost = createBlogPost(title, [], [])
+    const blogPost: BlogPost = createBlogPost(title)
       .addH2('h2見出し')
       .addParagraph('段落1')
       .addH3('h3見出し')
--- source/frontend/packages/web/src/entities/blogPost.ts
+++ source/frontend/packages/web/src/entities/blogPost.ts
@@ -1,15 +1,10 @@
 import type { Content } from '@/entities/postContants/content';
-import type { Heading } from './postContants/heading';
 import { createH1, createH2, createH3 } from './postContants/heading';
-import type { Paragraph } from './postContants/paragraph';
 import { createParagraph } from './postContants/paragraph';
 
 export type BlogPost = {
   getTitleText: () => string;
   getTitleLevel: () => number;
-  getH2List: () => Heading[];
-  getH3List: () => Heading[];
-  getParagraphList: () => Paragraph[];
   getContents: () => Content[];
   addH2: (text: string) => BlogPost;
   addH3: (text: string) => BlogPost;
@@ -18,25 +13,14 @@ export type BlogPost = {
 
 export const createBlogPost = (
   initialTitle: string,
-  initialH2List: string[] = [],
-  initialH3List: string[] = [],
-  initialParagraphList: string[] = [],
 ): BlogPost => {
   // setter 等が必要になった時のために変数に保持しておく
   const title = createH1(1, initialTitle);
-  const h2List = initialH2List.map((h2) => createH2(1, h2));
-  const h3List = initialH3List.map((h3) => createH3(1, h3));
-  const paragraphList = initialParagraphList.map((paragraph) =>
-    createParagraph(1, paragraph),
-  );
   const contents: Content[] = [];
 
   return {
     getTitleText: () => title.getContent(),
     getTitleLevel: () => title.getLevel(),
-    getH2List: () => h2List,
-    getH3List: () => h3List,
-    getParagraphList: () => paragraphList,
     getContents: () => contents,
     addH2(text: string) {
       const id = contents.length + 1;
