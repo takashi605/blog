6494475b201606589f030e1b9a14ed2a4370384b
takashi605 <95337577+takashi605@users.noreply.github.com>
2024-10-06 10:27:01 +0900

    テストコード内の変数名を修正 #84

--- source/frontend/packages/web/src/entities/postContants/heading.spec.ts
+++ source/frontend/packages/web/src/entities/postContants/heading.spec.ts
@@ -3,23 +3,23 @@ import { createH1, createH2, createH3 } from '@/entities/postContants/heading';
 
 describe('エンティティ: 見出し', () => {
   it('h1 見出しを生成できる', () => {
-    const heading: Heading = createH1(1, 'h1見出し');
-    expect(heading.getText()).toBe('h1見出し');
-    expect(heading.getLevel()).toBe(1);
-    expect(heading.getId()).toBe(1);
+    const h1: Heading = createH1(1, 'h1見出し');
+    expect(h1.getText()).toBe('h1見出し');
+    expect(h1.getLevel()).toBe(1);
+    expect(h1.getId()).toBe(1);
   });
 
   it('h2 見出しを生成できる', () => {
-    const heading: Heading = createH2(1, 'h2見出し');
-    expect(heading.getText()).toBe('h2見出し');
-    expect(heading.getLevel()).toBe(2);
-    expect(heading.getId()).toBe(1);
+    const h2: Heading = createH2(1, 'h2見出し');
+    expect(h2.getText()).toBe('h2見出し');
+    expect(h2.getLevel()).toBe(2);
+    expect(h2.getId()).toBe(1);
   });
 
   it('h3 見出しを生成できる', () => {
-    const heading: Heading = createH3(1, 'h3見出し');
-    expect(heading.getText()).toBe('h3見出し');
-    expect(heading.getLevel()).toBe(3);
-    expect(heading.getId()).toBe(1);
+    const h3: Heading = createH3(1, 'h3見出し');
+    expect(h3.getText()).toBe('h3見出し');
+    expect(h3.getLevel()).toBe(3);
+    expect(h3.getId()).toBe(1);
   });
 });
