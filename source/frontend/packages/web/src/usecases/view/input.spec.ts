075baf35ce56f95fc99c22f4a622b9a13de13cb6
takashi605 <95337577+takashi605@users.noreply.github.com>
2024-10-07 18:28:28 +0900

    ユースケースへの input を生成するファクトリ関数を作った #84

---
+++ source/frontend/packages/web/src/usecases/view/input.spec.ts
@@ -0,0 +1,31 @@
+import { createViewBlogPostInput } from './input';
+
+describe('ユースケース: 投稿記事生成のための入力値', () => {
+  it('メソッドチェーンによるファクトリ', () => {
+    const input = createViewBlogPostInput()
+      .setPostTitle('記事タイトル')
+      .addH2('h2見出し1')
+      .addH3('h3見出し1')
+      .addParagraph('段落1')
+      .addH3('h3見出し2')
+      .addParagraph('段落2');
+
+    expect(input.getPostTitle()).toBe('記事タイトル');
+    expect(input.getContents().length).toBe(5);
+
+    expect(input.getContents()[0].type).toBe('h2');
+    expect(input.getContents()[0].contentValue).toBe('h2見出し1');
+
+    expect(input.getContents()[1].type).toBe('h3');
+    expect(input.getContents()[1].contentValue).toBe('h3見出し1');
+
+    expect(input.getContents()[2].type).toBe('paragraph');
+    expect(input.getContents()[2].contentValue).toBe('段落1');
+
+    expect(input.getContents()[3].type).toBe('h3');
+    expect(input.getContents()[3].contentValue).toBe('h3見出し2');
+
+    expect(input.getContents()[4].type).toBe('paragraph');
+    expect(input.getContents()[4].contentValue).toBe('段落2');
+  });
+});
--- source/frontend/packages/web/src/usecases/view/input.ts
+++ source/frontend/packages/web/src/usecases/view/input.ts
@@ -4,4 +4,41 @@ export type ViewBlogPostInput = {
     type: string;
     contentValue: string;
   }[];
+  getPostTitle: () => string;
+  getContents: () => { type: string; contentValue: string }[];
+  addH2: (contentValue: string) => ViewBlogPostInput;
+  addH3: (contentValue: string) => ViewBlogPostInput;
+  addParagraph: (contentValue: string) => ViewBlogPostInput;
+  setPostTitle: (postTitle: string) => ViewBlogPostInput;
+};
+
+export const createViewBlogPostInput = (): ViewBlogPostInput => {
+  let postTitle = '';
+  const contents: {
+    type: string;
+    contentValue: string;
+  }[] = [];
+
+  return {
+    postTitle: '',
+    contents: [],
+    getPostTitle: () => postTitle,
+    getContents: () => contents,
+    addH2(contentValue: string) {
+      contents.push({ type: 'h2', contentValue });
+      return this;
+    },
+    addH3(contentValue: string) {
+      contents.push({ type: 'h3', contentValue });
+      return this;
+    },
+    addParagraph(contentValue: string) {
+      contents.push({ type: 'paragraph', contentValue });
+      return this;
+    },
+    setPostTitle(newPostTitle: string) {
+      postTitle = newPostTitle;
+      return this;
+    },
+  };
 };
