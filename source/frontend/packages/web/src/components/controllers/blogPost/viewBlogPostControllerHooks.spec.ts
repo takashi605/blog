efdbf9b7608a4ae73a7c84f7565f73ccfcdc92b5
takashi605 <95337577+takashi605@users.noreply.github.com>
2024-10-11 16:32:35 +0900

    h3,paragraph についても同じくテストを書いた #87

--- source/frontend/packages/web/src/components/controllers/blogPost/viewBlogPostControllerHooks.spec.ts
+++ source/frontend/packages/web/src/components/controllers/blogPost/viewBlogPostControllerHooks.spec.ts
@@ -40,5 +40,41 @@ describe('カスタムフック: useViewBlogPostController', () => {
         }
       });
     });
+
+    it('h3 が取得できる', async () => {
+      const { result } = renderHook(() => useViewBlogPostController(1));
+      await waitFor(() => {
+        if (result.current === null) {
+          throw new Error('投稿記事が取得できませんでした');
+        }
+        expect(result.current.contents).not.toHaveLength(0);
+        expectContainH3TypeContent();
+
+        function expectContainH3TypeContent() {
+          expect(
+            result.current.contents.some((content) => content.type === 'h3'),
+          ).toBe(true);
+        }
+      });
+    });
+
+    it('paragraph が取得できる', async () => {
+      const { result } = renderHook(() => useViewBlogPostController(1));
+      await waitFor(() => {
+        if (result.current === null) {
+          throw new Error('投稿記事が取得できませんでした');
+        }
+        expect(result.current.contents).not.toHaveLength(0);
+        expectContainParagraphTypeContent();
+
+        function expectContainParagraphTypeContent() {
+          expect(
+            result.current.contents.some(
+              (content) => content.type === 'paragraph',
+            ),
+          ).toBe(true);
+        }
+      });
+    });
   });
 });
