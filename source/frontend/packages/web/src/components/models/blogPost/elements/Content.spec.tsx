2362a0f355b47bff31a8f73c17cec73e17196eef
takashi605 <95337577+takashi605@users.noreply.github.com>
2024-10-11 17:13:16 +0900

    h3,p についてもテストした #87

--- source/frontend/packages/web/src/components/models/blogPost/elements/Content.spec.tsx
+++ source/frontend/packages/web/src/components/models/blogPost/elements/Content.spec.tsx
@@ -9,4 +9,18 @@ describe('コンポーネント: viewBlogPostController', () => {
     const h2 = screen.getByRole('heading', { level: 2 });
     expect(h2).toHaveTextContent('h2見出し');
   });
+
+  it('type に h3 を渡したとき、h3 タグを表示する', async () => {
+    render(<Content type="h3" value="h3見出し" />);
+
+    const h3 = screen.getByRole('heading', { level: 3 });
+    expect(h3).toHaveTextContent('h3見出し');
+  });
+
+  it('type に paragraph を渡したとき、p タグを表示する', async () => {
+    render(<Content type="paragraph" value="段落" />);
+
+    const p = screen.getByRole('paragraph');
+    expect(p).toHaveTextContent('段落');
+  });
 });
