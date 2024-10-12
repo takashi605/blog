7dd54352ef5b4563cf1b4ed00340b2217aab46a8
takashi605 <95337577+takashi605@users.noreply.github.com>
2024-10-12 11:24:45 +0900

    H3 コンポーネントを作成 #92

--- source/frontend/packages/web/src/components/models/blogPost/ui/contents/Content.tsx
+++ source/frontend/packages/web/src/components/models/blogPost/ui/contents/Content.tsx
@@ -1,11 +1,12 @@
 import type { ContentProps } from '@/components/models/blogPost/controllers/ViewBlogPostController';
 import H2 from '@/components/models/blogPost/ui/contents/elements/H2';
+import H3 from '@/components/models/blogPost/ui/contents/elements/H3';
 
 function ContentRenderer({ type, value }: ContentProps) {
   return type === 'h2' ? (
     <H2 type={type} value={value} />
   ) : type === 'h3' ? (
-    <h3>{value}</h3>
+    <H3 type={type} value={value} />
   ) : type === 'paragraph' ? (
     <p>{value}</p>
   ) : null;
---
+++ source/frontend/packages/web/src/components/models/blogPost/ui/contents/elements/H3.tsx
@@ -0,0 +1,7 @@
+import type { ContentProps } from '@/components/models/blogPost/controllers/ViewBlogPostController';
+
+export function H3({ value }: ContentProps) {
+  return <h3>{value}</h3>;
+}
+
+export default H3;
