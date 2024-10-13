0f6a0e8fda842d74e275716d18df305823bbff12
takashi605 <95337577+takashi605@users.noreply.github.com>
2024-10-13 14:13:19 +0900

    h1 にフォントを適用 #92

--- source/frontend/packages/web/src/app/layout.tsx
+++ source/frontend/packages/web/src/app/layout.tsx
@@ -19,6 +19,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+          (function(d) {
+            var config = {
+              kitId: 'pxq2mmy',
+              scriptTimeout: 3000,
+              async: true
+            },
+            h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+          })(document);
+        `,
+          }}
+        />
+      </head>
       <body className={inter.className}>
         <Header />
         <main className={styles.main}>{children}</main>
