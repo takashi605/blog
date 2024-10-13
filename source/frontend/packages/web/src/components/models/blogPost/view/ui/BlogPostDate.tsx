fa9241788a56fe9e5ef5dca58429c8930ec6c5e7
takashi605 <95337577+takashi605@users.noreply.github.com>
2024-10-13 13:35:09 +0900

    日付を太字にした #92

--- source/frontend/packages/web/src/components/models/blogPost/view/ui/BlogPostDate.tsx
+++ source/frontend/packages/web/src/components/models/blogPost/view/ui/BlogPostDate.tsx
@@ -1,8 +1,9 @@
 import type { BlogPostDateProps } from '@/components/models/blogPost/view/controllers/types';
+import styles from './blogPostDate.module.scss';
 
 function BlogPostDate({ label, date }: BlogPostDateProps) {
   return (
-    <p>
+    <p className={styles.date}>
       {label}:<time dateTime={date}>{date}</time>
     </p>
   );
