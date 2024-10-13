b7f88af8ff189fc18b450a0ccbe674f90d47e660
takashi605 <95337577+takashi605@users.noreply.github.com>
2024-10-13 13:47:25 +0900

    h2 のスタイリングをした #92

--- source/frontend/packages/web/src/components/models/blogPost/view/ui/contents/elements/H2.tsx
+++ source/frontend/packages/web/src/components/models/blogPost/view/ui/contents/elements/H2.tsx
@@ -1,7 +1,8 @@
 import type { ContentElementProps } from '@/components/models/blogPost/view/ui/contents/Content';
+import styles from './h2.module.scss';
 
 export function H2({ children }: ContentElementProps) {
-  return <h2>{children}</h2>;
+  return <h2 className={styles.h2}>{children}</h2>;
 }
 
 export default H2;
