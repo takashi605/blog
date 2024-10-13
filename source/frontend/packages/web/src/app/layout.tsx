3293b80b39b053566573711d150a1cd0b9251390
takashi605 <95337577+takashi605@users.noreply.github.com>
2024-10-13 12:19:47 +0900

    Header コンポーネントを作成。作り込みはまだ #92

--- source/frontend/packages/web/src/app/layout.tsx
+++ source/frontend/packages/web/src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Header from '@/components/topLayout/header/Header';
 import 'destyle.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
@@ -17,7 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
