6149d519897d40113a55f320f987ea655005ddaf
takashi605 <95337577+takashi605@users.noreply.github.com>
2024-10-13 13:30:23 +0900

    フォーマットをかけた #92

--- source/frontend/packages/web/src/app/layout.tsx
+++ source/frontend/packages/web/src/app/layout.tsx
@@ -1,8 +1,8 @@
 import Header from '@/components/topLayout/header/Header';
 import 'destyle.css';
-import styles from './layout.module.scss';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import styles from './layout.module.scss';
 
 // フォントの定義
 const inter = Inter({ subsets: ['latin'] });
