c5be3598a4d998acf821f1abde8115c5976f9a3f
takashi605 <95337577+takashi605@users.noreply.github.com>
2024-10-13 13:29:27 +0900

    ヘッダーの余白を調整 #92

--- source/frontend/packages/web/src/components/topLayout/header/Header.tsx
+++ source/frontend/packages/web/src/components/topLayout/header/Header.tsx
@@ -2,9 +2,11 @@ import styles from './header.module.scss';
 
 function Header() {
   return (
-    <header className={styles.header}>
-      <div className={styles.blogTitle}>鉄火ブログ</div>
-    </header>
+    <div className={styles.headerWrapper}>
+      <header className={styles.header}>
+        <div className={styles.blogTitle}>鉄火ブログ</div>
+      </header>
+    </div>
   );
 }
 
