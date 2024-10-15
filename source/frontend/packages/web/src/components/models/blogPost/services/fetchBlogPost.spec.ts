0b156dd2e2c362ce4081cc10ee8c48de67d4b889
takashi605 <95337577+takashi605@users.noreply.github.com>
2024-10-15 20:06:38 +0900

    一部テストはメンテナンス性が低いと判断したため削除 #94

--- source/frontend/packages/web/src/components/models/blogPost/services/fetchBlogPost.spec.ts
+++ source/frontend/packages/web/src/components/models/blogPost/services/fetchBlogPost.spec.ts
@@ -1,9 +1,5 @@
 import { mockApiForServer } from '@/apiMock/serverForNode';
-import {
-  fetchBlogPost,
-  fetchRawBlogPost,
-} from '@/components/models/blogPost/services/fetchBlogPost';
-import { responseToViewBlogPost } from '@/components/models/blogPost/services/response';
+import { fetchBlogPost } from '@/components/models/blogPost/services/fetchBlogPost';
 
 beforeAll(() => {
   mockApiForServer.listen();
@@ -26,31 +22,5 @@ describe('投稿記事を取得する', () => {
 
     await expect(blogPostResponse.title).not.toBe('');
     await expect(blogPostResponse.contents.length).toBeGreaterThan(0);
-  });
-
-  // 以下は詳細な変換処理のテストであり、取得処理の振る舞いの理解としては上記のテストで十分
-  it('レスポンスをブログ記事の構造に変換できる', async () => {
-    const BlogPostResponse = await fetchRawBlogPost(1);
-
-    const blogPost = responseToViewBlogPost(BlogPostResponse);
-
-    expect(blogPost.title).toBe(BlogPostResponse.title);
-
-    if (
-      blogPost.contents.length === 0 ||
-      BlogPostResponse.contents.length === 0
-    ) {
-      throw new Error(
-        '投稿記事のテストデータにコンテンツが存在内ため正しくテストできません',
-      );
-    }
-
-    expect(blogPost.contents.length).toBe(BlogPostResponse.contents.length);
-
-    blogPost.contents.forEach((content, index) => {
-      expect(content.id).toBe(index + 1);
-      expect(content.value).toBe(BlogPostResponse.contents[index].value);
-      expect(content.type).toBe(BlogPostResponse.contents[index].type);
-    });
   });
 });
