2fec1974f337544f601d47720383c244f49fcadf
takashi605 <95337577+takashi605@users.noreply.github.com>
2024-10-16 19:08:04 +0900

    日付変換関数のエラーハンドリング箇所を切り出してリファクタリングした #98

--- source/frontend/packages/web/src/usecases/view/output.ts
+++ source/frontend/packages/web/src/usecases/view/output.ts
@@ -1,3 +1,6 @@
+import { formatDate2DigitString } from '@/utils/date';
+import type { BlogPost } from 'entities/src/blogPost';
+
 export type ViewBlogPost = {
   readonly title: string;
   readonly postDate: string;
@@ -7,4 +10,26 @@ export type ViewBlogPost = {
     readonly value: string;
     readonly type: string;
   }>;
+};
+
+export const extractPostDateForDTO = (blogPost: BlogPost): string => {
+  const postDateValue = blogPost.getPostDate();
+  if (!postDateValue) {
+    throw new Error(
+      '投稿日は必須ですが、投稿日が存在しない記事を生成しようとしました',
+    );
+  }
+  const postDate = formatDate2DigitString(postDateValue);
+  return postDate;
+};
+
+export const extractLastUpdateDateForDTO = (blogPost: BlogPost): string => {
+  const lastUpdateDateValue = blogPost.getLastUpdateDate();
+  if (!lastUpdateDateValue) {
+    throw new Error(
+      '最終更新日は必須ですが、最終更新日が存在しない記事を生成しようとしました',
+    );
+  }
+  const lastUpdateDate = formatDate2DigitString(lastUpdateDateValue);
+  return lastUpdateDate;
 };
--- source/frontend/packages/web/src/usecases/view/viewBlogPost.ts
+++ source/frontend/packages/web/src/usecases/view/viewBlogPost.ts
@@ -1,31 +1,18 @@
 import type { ViewBlogPostInput } from '@/usecases/view/input/input';
-import type { ViewBlogPost } from '@/usecases/view/output';
-import { formatDate2DigitString } from '@/utils/date';
+import {
+  extractLastUpdateDateForDTO,
+  extractPostDateForDTO,
+  type ViewBlogPost,
+} from '@/usecases/view/output';
 import type { BlogPost } from 'entities/src/blogPost';
 
 export const viewBlogPost = (input: ViewBlogPostInput): ViewBlogPost => {
   const blogPost: BlogPost = input.generateBlogPost();
 
-  const postDateValue = blogPost.getPostDate();
-  if (!postDateValue) {
-    throw new Error(
-      '投稿日は必須ですが、投稿日が存在しない記事を生成しようとしました',
-    );
-  }
-  const postDate = formatDate2DigitString(postDateValue);
-
-  const lastUpdateDateValue = blogPost.getLastUpdateDate();
-  if (!lastUpdateDateValue) {
-    throw new Error(
-      '最終更新日は必須ですが、最終更新日が存在しない記事を生成しようとしました',
-    );
-  }
-  const lastUpdateDate = formatDate2DigitString(lastUpdateDateValue);
-
   return {
     title: blogPost.getTitleText(),
-    postDate,
-    lastUpdateDate,
+    postDate: extractPostDateForDTO(blogPost),
+    lastUpdateDate: extractLastUpdateDateForDTO(blogPost),
     contents: blogPost.getContents().map((content, index) => {
       return {
         id: index + 1,
