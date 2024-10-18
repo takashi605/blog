d2c13ddbe6f54eac5c29c606e260e8015ca4d132
takashi605 <95337577+takashi605@users.noreply.github.com>
2024-10-19 08:21:30 +0900

    投稿記事の関連日付を扱う class を作った #98

--- source/frontend/packages/entities/src/blogPost/blogPostDate.spec.ts
+++ source/frontend/packages/entities/src/blogPost/blogPostDate.spec.ts
@@ -1,7 +1,7 @@
 import { EntityError } from '../error/error';
-import { createBlogPostDate } from './blogPostDate';
+import { createBlogPostDate, n__BlogPostDate } from './blogPostDate';
 
-describe('エンティティ：投稿記事の日付', () => {
+describe('【旧】エンティティ：投稿記事の日付', () => {
   it('日付を生成できる', () => {
     const blogPostDate = createBlogPostDate('2021-01-01');
 
@@ -24,6 +24,41 @@ describe('エンティティ：投稿記事の日付', () => {
   it('エラーの際、EntityError 型のエラーが発生する', () => {
     try {
       createBlogPostDate('');
+    } catch (error) {
+      expect(error instanceof EntityError).toBeTruthy();
+    }
+  });
+});
+
+describe('エンティティ：投稿記事の日付', () => {
+  it('日付を生成できる', () => {
+    const blogPostDate = new n__BlogPostDate('2021-01-01');
+
+    const expectedDate = new Date('2021-01-01');
+    expect(blogPostDate.getDate()).toEqual(expectedDate);
+  });
+
+  it('日付を YYYY/MM/DD の形式で取得できる', () => {
+    const blogPostDate = new n__BlogPostDate('2021-01-01');
+
+    expect(blogPostDate.format2DigitString()).toEqual('2021/01/01');
+  });
+
+  it('YYYY-MM-DD の形式ではない文字列を渡すと EntityError が発生する', () => {
+    expect(() => new n__BlogPostDate('')).toThrow(
+      '日付は YYYY-MM-DD 形式で指定してください',
+    );
+    expect(() => new n__BlogPostDate('2021-01-01 00:00:00')).toThrow(
+      '日付は YYYY-MM-DD 形式で指定してください',
+    );
+    expect(() => new n__BlogPostDate('2021/01/01')).toThrow(
+      '日付は YYYY-MM-DD 形式で指定してください',
+    );
+  });
+
+  it('エラーの際、EntityError 型のエラーが発生する', () => {
+    try {
+      new n__BlogPostDate('');
     } catch (error) {
       expect(error instanceof EntityError).toBeTruthy();
     }
--- source/frontend/packages/entities/src/blogPost/blogPostDate.ts
+++ source/frontend/packages/entities/src/blogPost/blogPostDate.ts
@@ -16,3 +16,27 @@ export const createBlogPostDate = (dateString: string): BlogPostDate => {
     getDate: () => value,
   };
 };
+
+export class n__BlogPostDate {
+  date: Date;
+
+  constructor(dateString: string) {
+    if (!dateString.match(/^\d{4}-\d{2}-\d{2}$/)) {
+      throw new EntityError('日付は YYYY-MM-DD 形式で指定してください');
+    }
+    this.date = new Date(dateString);
+  }
+
+  format2DigitString(): string {
+    const options: Intl.DateTimeFormatOptions = {
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+    };
+    return this.date.toLocaleDateString('ja-JP', options);
+  }
+
+  getDate(): Date {
+    return this.date;
+  }
+}
