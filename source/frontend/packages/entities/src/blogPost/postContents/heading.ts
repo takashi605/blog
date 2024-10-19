5d7c0d8ebee435d1be6399fff773431e9fec550f
takashi605 <95337577+takashi605@users.noreply.github.com>
2024-10-19 09:17:33 +0900

    Heading をクラスで表現できるようにした #98

--- source/frontend/packages/entities/src/blogPost/postContents/heading.ts
+++ source/frontend/packages/entities/src/blogPost/postContents/heading.ts
@@ -1,32 +1,38 @@
 import type { Content } from './content';
 
-export type Heading = Content & {
-  getLevel: () => number;
-};
-
 export const createH1 = (id: number, initialText: string): Heading => {
-  return createHeading(id, initialText, 1);
+  return new Heading(id, initialText, 1);
 };
 
 export const createH2 = (id: number, initialText: string): Heading => {
-  return createHeading(id, initialText, 2);
+  return new Heading(id, initialText, 2);
 };
 
 export const createH3 = (id: number, initialText: string): Heading => {
-  return createHeading(id, initialText, 3);
+  return new Heading(id, initialText, 3);
 };
 
-const createHeading = (
-  id: number,
-  initialText: string,
-  level: number,
-): Heading => {
-  const text = initialText;
+export class Heading implements Content {
+  private id: number;
+  private text: string;
+  private level: number;
 
-  return {
-    getId: () => id,
-    getValue: () => text,
-    getLevel: () => level,
-    getType: () => `h${level}`,
-  };
-};
+  constructor(id: number, text: string, level: number) {
+    this.id = id;
+    this.text = text;
+    this.level = level;
+  }
+
+  getId() {
+    return this.id;
+  }
+  getValue() {
+    return this.text;
+  }
+  getType() {
+    return `h${this.level}`;
+  }
+  getLevel() {
+    return this.level;
+  }
+}
