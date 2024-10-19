bb0cee7250110f76db749d49e4739256ac0be865
takashi605 <95337577+takashi605@users.noreply.github.com>
2024-10-19 19:51:16 +0900

    ユースケース層のカスタムエラーを利用するようにした #98

--- source/frontend/packages/web/src/usecases/view/output/dto.ts
+++ source/frontend/packages/web/src/usecases/view/output/dto.ts
@@ -1,3 +1,4 @@
+import { UsecaseError } from '@/usecases/error';
 import { formatDate2DigitString } from '@/utils/date';
 import type { BlogPost } from 'entities/src/blogPost/index';
 import type { Content } from 'entities/src/blogPost/postContents/content';
@@ -26,7 +27,7 @@ export class BlogPostDTOBuilder {
     try {
       return formatDate2DigitString(this.blogPost.getPostDate());
     } catch {
-      throw new Error('投稿日が存在しない記事を生成しようとしました');
+      throw new UsecaseError('投稿日が存在しない記事を生成しようとしました');
     }
   }
 
@@ -34,7 +35,9 @@ export class BlogPostDTOBuilder {
     try {
       return formatDate2DigitString(this.blogPost.getLastUpdateDate());
     } catch {
-      throw new Error('最終更新日が存在しない記事を生成しようとしました');
+      throw new UsecaseError(
+        '最終更新日が存在しない記事を生成しようとしました',
+      );
     }
   }
 
