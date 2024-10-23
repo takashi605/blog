ec13aee4c909b817ff7d22f53e833ab8f4a847e2
takashi605 <95337577+takashi605@users.noreply.github.com>
2024-10-23 19:35:38 +0900

    jest.config をコピペ #111

---
+++ source/frontend/packages/blog-admin/jest.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from 'jest';
+import nextJest from 'next/jest';
+
+const createJestConfig = nextJest({
+  // テスト環境でnext.config.jsと.envファイルを読み込むために、Next.jsアプリへのパスを指定
+  dir: './',
+});
+
+const config: Config = {
+  coverageProvider: 'v8',
+  testEnvironment: 'jsdom',
+  testEnvironmentOptions: {
+    customExportConditions: [],
+  },
+  setupFiles: ['./jest.polyfills.mjs'],
+};
+
+export default createJestConfig(config);
