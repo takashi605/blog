e3970d1a2ed28150f877d67192317f75f9c762f2
takashi605 <95337577+takashi605@users.noreply.github.com>
2024-10-24 16:42:22 +0900

    不要になった型や関数を削除 #109

--- source/frontend/packages/web/src/usecases/entityBuilder/content.spec.ts
+++ source/frontend/packages/web/src/usecases/entityBuilder/content.spec.ts
@@ -1,22 +1,7 @@
-import {
-  ContentBuilder,
-  createContent,
-  type ContentForBlogPostBuilder,
-} from '@/usecases/entityBuilder/content';
+import { ContentBuilder } from '@/usecases/entityBuilder/content';
 import { ContentType } from 'entities/src/blogPost/postContents/content';
 
 describe('ユースケース: 投稿記事の閲覧', () => {
-  it('【旧】id およびユースケースへの入力値からコンテントを生成できる', () => {
-    const contentInput: ContentForBlogPostBuilder = {
-      type: ContentType.H2,
-      contentValue: 'h2見出し',
-    };
-    const content = createContent(1, contentInput);
-    expect(content.getValue()).toBe('h2見出し');
-    expect(content.getType()).toBe('h2');
-    expect(content.getId()).toBe(1);
-  });
-
   it('id およびユースケースへの入力値からコンテントを生成できる', () => {
     const contentBuilder = new ContentBuilder({
       type: ContentType.H3,
--- source/frontend/packages/web/src/usecases/entityBuilder/content.ts
+++ source/frontend/packages/web/src/usecases/entityBuilder/content.ts
@@ -3,22 +3,6 @@ import type {
   ContentType,
 } from 'entities/src/blogPost/postContents/content';
 
-export type ContentForBlogPostBuilder = {
-  type: ContentType;
-  contentValue: string;
-};
-
-export const createContent = (
-  id: number,
-  input: ContentForBlogPostBuilder,
-): Content => {
-  return {
-    getId: () => id,
-    getValue: () => input.contentValue,
-    getType: () => input.type,
-  };
-};
-
 export class ContentBuilder {
   private type: ContentType;
   private contentValue: string;
