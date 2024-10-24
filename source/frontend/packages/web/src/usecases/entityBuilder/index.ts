161e66d35db175ac2783822ca704a741bb8a5da4
takashi605 <95337577+takashi605@users.noreply.github.com>
2024-10-24 16:41:35 +0900

    createContent を ContentBuilder.createContent() で利用できるようにした #109

--- source/frontend/packages/web/src/usecases/entityBuilder/index.ts
+++ source/frontend/packages/web/src/usecases/entityBuilder/index.ts
@@ -1,7 +1,4 @@
-import {
-  createContent,
-  type ContentForBlogPostBuilder,
-} from '@/usecases/entityBuilder/content';
+import { ContentBuilder } from '@/usecases/entityBuilder/content';
 import { BlogPost } from 'entities/src/blogPost/index';
 import { ContentType } from 'entities/src/blogPost/postContents/content';
 
@@ -9,7 +6,7 @@ export class BlogPostBuilder {
   private postTitle = '';
   private postDate = '';
   private lastUpdateDate = '';
-  private contents: ContentForBlogPostBuilder[] = [];
+  private contents: ContentBuilder[] = [];
 
   setPostTitle(postTitle: string) {
     this.postTitle = postTitle;
@@ -27,24 +24,30 @@ export class BlogPostBuilder {
   }
 
   addH2(contentValue: string) {
-    this.contents.push({ type: ContentType.H2, contentValue });
+    this.contents.push(
+      new ContentBuilder({ type: ContentType.H2, contentValue }),
+    );
     return this;
   }
 
   addH3(contentValue: string) {
-    this.contents.push({ type: ContentType.H3, contentValue });
+    this.contents.push(
+      new ContentBuilder({ type: ContentType.H3, contentValue }),
+    );
     return this;
   }
 
   addParagraph(contentValue: string) {
-    this.contents.push({ type: ContentType.Paragraph, contentValue });
+    this.contents.push(
+      new ContentBuilder({ type: ContentType.Paragraph, contentValue }),
+    );
     return this;
   }
 
   injectionContentsTo(blogPost: BlogPost) {
     this.contents.forEach((content) => {
       blogPost.addContent(
-        createContent(blogPost.getContents().length + 1, content),
+        content.createContent(blogPost.getContents().length + 1),
       );
     });
   }
