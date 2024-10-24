fafa2e5424b4a603818abe967c8819ae3334329f
takashi605 <95337577+takashi605@users.noreply.github.com>
2024-10-24 20:46:54 +0900

    振る舞いとして、記事の生成を追加した #109

--- source/frontend/packages/blog-admin/src/usecases/createBlogPost.spec.ts
+++ source/frontend/packages/blog-admin/src/usecases/createBlogPost.spec.ts
@@ -2,7 +2,7 @@ import { createBlogPostBuilder } from 'entities/src/blogPost/blogPostBuilder';
 import { BlogPostCreator } from './createBlogPost';
 
 describe('ユースケース: 記事の投稿', () => {
-  it('ブログ記事のビルダーを扱うクラスを生成できる', () => {
+  it('ブログ記事のデータを生成できる', () => {
     const builder = createBlogPostBuilder()
       .setPostTitle('記事タイトル')
       .setPostDate('2021-01-01')
@@ -11,6 +11,7 @@ describe('ユースケース: 記事の投稿', () => {
       .addH3('h3見出し1')
       .addParagraph('段落1');
     const blogPostCreator = new BlogPostCreator(builder);
-    console.log(blogPostCreator);
+    const blogPost = blogPostCreator.buildBlogPost();
+    expect(blogPost.getTitleText()).toBe('記事タイトル');
   });
 });
--- source/frontend/packages/blog-admin/src/usecases/createBlogPost.ts
+++ source/frontend/packages/blog-admin/src/usecases/createBlogPost.ts
@@ -1,8 +1,13 @@
+import type { BlogPost } from 'entities/src/blogPost';
 import type { BlogPostBuilder } from 'entities/src/blogPost/blogPostBuilder';
 
 export class BlogPostCreator {
   private builder: BlogPostBuilder;
   constructor(builder: BlogPostBuilder) {
     this.builder = builder;
+  }
+
+  buildBlogPost(): BlogPost {
+    return this.builder.build();
   }
 }
