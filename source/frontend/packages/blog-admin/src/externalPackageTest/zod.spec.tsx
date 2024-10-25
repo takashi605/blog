153473630b1bd9b8f5d6e6888143223d15c0f60a
takashi605 <95337577+takashi605@users.noreply.github.com>
2024-10-26 08:17:44 +0900

    スキーマから型を生成できることをテストした #116

--- source/frontend/packages/blog-admin/src/externalPackageTest/zod.spec.tsx
+++ source/frontend/packages/blog-admin/src/externalPackageTest/zod.spec.tsx
@@ -17,4 +17,16 @@ describe('学習用テスト： Zod パッケージ', () => {
     expect(objectSchema.parse({ name: 'Alice', age: 20 })).toEqual({ name: 'Alice', age: 20 });
     expect(() => objectSchema.parse({ name: 'Alice', age: '20' })).toThrow();
   });
+
+  it('スキーマから型を生成できる', () => {
+    const objectSchema = z.object({
+      name: z.string(),
+      age: z.number(),
+    });
+
+    type Person = z.infer<typeof objectSchema>;
+
+    const person: Person = { name: 'Alice', age: 20 };
+    expect(() => objectSchema.parse(person)).not.toThrow();
+  });
 });
