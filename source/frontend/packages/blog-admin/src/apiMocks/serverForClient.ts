eaf1892a40132a3750016126915f77a18c2467a9
takashi605 <95337577+takashi605@users.noreply.github.com>
2024-10-26 07:04:55 +0900

    rename dir apiMock->apiMocks #116

--- source/frontend/packages/blog-admin/src/apiMocks/serverForClient.ts
+++ source/frontend/packages/blog-admin/src/apiMocks/serverForClient.ts
@@ -1,4 +1,4 @@
-import { allHandlers } from '@/apiMock/handlers';
+import { allHandlers } from '@/apiMocks/handlers';
 import { setupWorker } from 'msw/browser';
 
 export const mockApiForClient = setupWorker(...allHandlers);
