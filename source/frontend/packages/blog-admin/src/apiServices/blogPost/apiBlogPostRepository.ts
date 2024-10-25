53046b42d120e4e721c1cf31b1bd660f1ce8c968
takashi605 <95337577+takashi605@users.noreply.github.com>
2024-10-26 07:50:53 +0900

    post 送信を別関数に切り出した #116

--- source/frontend/packages/blog-admin/src/apiServices/blogPost/apiBlogPostRepository.ts
+++ source/frontend/packages/blog-admin/src/apiServices/blogPost/apiBlogPostRepository.ts
@@ -9,13 +9,7 @@ type BlogPostResponse = {
 
 export class ApiBlogPostRepository implements BlogPostRepository {
   async save(blogPost: string): Promise<BlogPostResponse> {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/posts`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: blogPost,
-    });
+    const response = await post(blogPost);
 
     if (!response.ok) {
       throw new Error('ブログ記事の保存に失敗しました');
@@ -24,3 +18,14 @@ export class ApiBlogPostRepository implements BlogPostRepository {
     return response.json();
   }
 }
+
+const post = async (blogPost: string): Promise<Response> => {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/posts`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: blogPost,
+  });
+  return response;
+};
