c514c073bd0f14606a94b862ae1cc95f3e939d7b
takashi605 <95337577+takashi605@users.noreply.github.com>
2024-10-27 08:58:28 +0900

    react-hook-form を使ってフォームデータを管理できるようにした #118

--- source/frontend/packages/blog-admin/src/models/blogPost/create/CreateBlogPostForm.tsx
+++ source/frontend/packages/blog-admin/src/models/blogPost/create/CreateBlogPostForm.tsx
@@ -1,10 +1,20 @@
-import { createBlogPostAction } from '@/models/blogPost/create/formAction';
+import type { CreateBlogPostFormData } from '@/models/blogPost/create/formAction';
+import {
+  createBlogPostAction,
+  createBlogPostFormSchema,
+} from '@/models/blogPost/create/formAction';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
 
 function CreateBlogPostForm() {
+  const { register, handleSubmit } = useForm<CreateBlogPostFormData>({
+    resolver: zodResolver(createBlogPostFormSchema),
+  });
+
   return (
-    <form role="form" onSubmit={createBlogPostAction}>
+    <form role="form" onSubmit={handleSubmit(createBlogPostAction)}>
       <label htmlFor="title">タイトル</label>
-      <input type="text" id="title" name="title" />
+      <input id="title" {...register('title')} />
       <button type="submit">投稿</button>
     </form>
   );
--- source/frontend/packages/blog-admin/src/models/blogPost/create/formAction.ts
+++ source/frontend/packages/blog-admin/src/models/blogPost/create/formAction.ts
@@ -1,28 +1,20 @@
 import { ApiBlogPostRepository } from '@/apiServices/blogPost/apiBlogPostRepository';
 import { BlogPostCreator } from '@/usecases/create/createBlogPost';
 import { createBlogPostBuilder } from 'entities/src/blogPost/blogPostBuilder';
-import type { FormEvent } from 'react';
+import type { SubmitHandler } from 'react-hook-form';
 import { z } from 'zod';
-import { parseWithZod } from '@conform-to/zod';
 
-const createBlogPostFormDataSchema = z.object({
+export const createBlogPostFormSchema = z.object({
   title: z.string(),
 });
 
-export const createBlogPostAction = async (event: FormEvent) => {
-  event.preventDefault();
+export type CreateBlogPostFormData = z.infer<typeof createBlogPostFormSchema>;
 
-  const formData = new FormData(event.target as HTMLFormElement);
-  const submission = parseWithZod(formData, {
-    schema: createBlogPostFormDataSchema,
-  });
-
-  if (submission.status !== 'success') {
-    return submission.reply();
-  }
-
+export const createBlogPostAction: SubmitHandler<
+  CreateBlogPostFormData
+> = async (formData) => {
   const blogPostBuilder = createBlogPostBuilder()
-    .setPostTitle(submission.value.title)
+    .setPostTitle(formData.title)
     .addH2('h2見出し1')
     .addH3('h3見出し1')
     .addParagraph('段落1');
