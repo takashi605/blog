442df0d7d06f6c50bb1e68945d3e40375c7c1e22
takashi605 <95337577+takashi605@users.noreply.github.com>
2024-10-27 09:41:26 +0900

    formAction をリファクタリング #118

--- source/frontend/packages/blog-admin/src/models/blogPost/create/formAction.ts
+++ source/frontend/packages/blog-admin/src/models/blogPost/create/formAction.ts
@@ -1,5 +1,6 @@
 import { ApiBlogPostRepository } from '@/apiServices/blogPost/apiBlogPostRepository';
 import { BlogPostCreator } from '@/usecases/create/createBlogPost';
+import type { BlogPostBuilder } from 'entities/src/blogPost/blogPostBuilder';
 import { createBlogPostBuilder } from 'entities/src/blogPost/blogPostBuilder';
 import type { SubmitHandler } from 'react-hook-form';
 import { z } from 'zod';
@@ -19,23 +20,43 @@ export type CreateBlogPostFormData = z.infer<typeof createBlogPostFormSchema>;
 export const createBlogPostAction: SubmitHandler<
   CreateBlogPostFormData
 > = async (formData) => {
-  const blogPostBuilder = createBlogPostBuilder()
-    .setPostTitle(formData.title)
+  const blogPostBuilder = createBlogPostBuilder();
+  injectFormDataToBuilder(formData, blogPostBuilder);
 
-  formData.contents.forEach((content) => {
-    if (content.type === 'h2') {
-      blogPostBuilder.addH2(content.text);
-    } else if (content.type === 'h3') {
-      blogPostBuilder.addH3(content.text);
-    } else if (content.type === 'paragraph') {
-      blogPostBuilder.addParagraph(content.text);
-    }
-  });
-
-  const blogPostRepository = new ApiBlogPostRepository();
-  const blogPostCreator = new BlogPostCreator(
-    blogPostBuilder,
-    blogPostRepository,
-  );
+  const blogPostCreator = setupBlogPostCreator(blogPostBuilder);
   await blogPostCreator.execute();
 };
+
+function setupBlogPostCreator(
+  builder: BlogPostBuilder
+) {
+  const repository = new ApiBlogPostRepository();
+  return new BlogPostCreator(builder, repository);
+}
+
+function injectFormDataToBuilder(
+  formData: CreateBlogPostFormData,
+  builder: BlogPostBuilder,
+) {
+  builder.setPostTitle(formData.title);
+  injectContentsToBuilder(formData, builder);
+}
+
+function injectContentsToBuilder(
+  formData: CreateBlogPostFormData,
+  builder: BlogPostBuilder,
+) {
+  formData.contents.forEach((content) => {
+    switch (content.type) {
+      case 'h2':
+        builder.addH2(content.text);
+        break;
+      case 'h3':
+        builder.addH3(content.text);
+        break;
+      case 'paragraph':
+        builder.addParagraph(content.text);
+        break;
+    }
+  });
+}
