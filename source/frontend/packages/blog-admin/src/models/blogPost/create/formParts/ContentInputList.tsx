e7cd1989bd2cd70fef5b3df70ba5c92d2beb81a2
takashi605 <95337577+takashi605@users.noreply.github.com>
2024-10-28 20:21:04 +0900

    ContentInputList をリファクタリング #118

--- source/frontend/packages/blog-admin/src/models/blogPost/create/formParts/ContentInputList.tsx
+++ source/frontend/packages/blog-admin/src/models/blogPost/create/formParts/ContentInputList.tsx
@@ -2,20 +2,31 @@ import { useFieldArrayFormContext } from '@/components/form/FieldArrayFormProvid
 import type { CreateBlogPostFormData } from '@/models/blogPost/create/formSchema';
 
 export default function ContentInputList() {
-  const { register, fields } =
-    useFieldArrayFormContext<CreateBlogPostFormData>();
+  const { fields } = useFieldArrayFormContext<CreateBlogPostFormData>();
 
   return (
     <>
       {fields.map((field, index) => (
-        <div key={field.id}>
-          <label htmlFor={`contents.${index}.text`}>{field.type}</label>
-          <input
-            id={`contents.${index}.text`}
-            {...register(`contents.${index}.text` as const)}
-          />
-        </div>
+        <ContentInput
+          key={field.id}
+          inputId={`contents.${index}.text`}
+          contentType={field.type}
+        />
       ))}
     </>
+  );
+}
+
+type ContentInputProps = {
+  inputId: `contents.${number}.text`;
+  contentType: string;
+};
+function ContentInput({ inputId, contentType }: ContentInputProps) {
+  const { register } = useFieldArrayFormContext<CreateBlogPostFormData>();
+  return (
+    <div>
+      <label htmlFor={inputId}>{contentType}</label>
+      <input id={inputId} {...register(inputId)} />
+    </div>
   );
 }
