b9d21b874d4991f4c44c847e73d989db5d7d608b
takashi605 <95337577+takashi605@users.noreply.github.com>
2024-10-31 08:25:36 +0900

    MSW が本番環境でも動作するようにした #107

--- source/frontend/packages/blog-admin/src/components/MSWInitializer.tsx
+++ source/frontend/packages/blog-admin/src/components/MSWInitializer.tsx
@@ -4,12 +4,10 @@ import { useEffect } from 'react';
 
 export function MSWInitializer() {
   useEffect(() => {
-    if (process.env.NODE_ENV === 'development') {
-      (async () => {
-        const { mockApiForClient } = await import('@/apiMocks/serverForClient');
-        mockApiForClient.start();
-      })();
-    }
+    (async () => {
+      const { mockApiForClient } = await import('@/apiMocks/serverForClient');
+      mockApiForClient.start();
+    })();
   }, []);
 
   return null;
