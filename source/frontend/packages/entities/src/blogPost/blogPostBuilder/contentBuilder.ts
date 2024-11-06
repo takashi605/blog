9be2aa899f628ec7fb3fdca5327f2144d2c2c7e5
takashi605 <95337577+takashi605@users.noreply.github.com>
2024-11-06 18:46:08 +0900

    ContentInput 型を定義 #131

--- source/frontend/packages/entities/src/blogPost/blogPostBuilder/contentBuilder.ts
+++ source/frontend/packages/entities/src/blogPost/blogPostBuilder/contentBuilder.ts
@@ -8,7 +8,7 @@ export class ContentBuilder {
   private type: ContentType;
   private contentValue: string;
 
-  constructor(input: { type: ContentType; contentValue: string }) {
+  constructor(input: old__ContentInput) {
     this.type = input.type;
     this.contentValue = input.contentValue;
   }
@@ -17,3 +17,11 @@ export class ContentBuilder {
     return createContent({ id, type: this.type, value: this.contentValue });
   }
 }
+
+type old__ContentInput = HeadingInput | ParagraphInput;
+type ContentInput = HeadingInput | ParagraphInput | ImageInput;
+type ContentInputBase = { type: ContentType };
+// TODO rename contentValue->text
+type HeadingInput = { contentValue: string } & ContentInputBase;
+type ParagraphInput = { contentValue: string } & ContentInputBase;
+type ImageInput = { path: string } & ContentInputBase;
