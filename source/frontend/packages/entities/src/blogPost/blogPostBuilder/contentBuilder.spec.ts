141749fd12fe6c15d8ddf7389011ddc4662c543d
takashi605 <95337577+takashi605@users.noreply.github.com>
2024-11-08 16:40:41 +0900

    contentBuilder の拡張がうまくいかなかったので組みなおし。h2 を生成するストラテジーを作成した #131

--- source/frontend/packages/entities/src/blogPost/blogPostBuilder/contentBuilder.spec.ts
+++ source/frontend/packages/entities/src/blogPost/blogPostBuilder/contentBuilder.spec.ts
@@ -1,12 +1,6 @@
 import { ContentType } from '../postContents/content';
 import type { Heading } from '../postContents/heading';
-import {
-  ContentBuilder,
-  H2BuildStrategy,
-  H3BuildStrategy,
-  ImageBuildStrategy,
-  ParagraphBuildStrategy,
-} from './contentBuilder';
+import { ContentBuilder, H2Input, StrategyContext } from './contentBuilder';
 
 describe('エンティティ: 投稿記事の閲覧', () => {
   it('id およびユースケースへの入力値からコンテントを生成できる', () => {
@@ -21,38 +15,36 @@ describe('エンティティ: 投稿記事の閲覧', () => {
   });
 
   it('h2 を生成するストラテジー', () => {
-    const h2BuildStrategy = new H2BuildStrategy({
-      type: ContentType.H2,
-      contentValue: 'h2見出し',
-    });
-    const h2 = h2BuildStrategy.build(1);
+    const h2Input = new H2Input('h2見出し');
+    const strategyContext = new StrategyContext(h2Input);
+    const h2 = strategyContext.build(1);
     expect(h2.getValue()).toBe('h2見出し');
   });
 
-  it('h3 を生成するストラテジー', () => {
-    const h3BuildStrategy = new H3BuildStrategy({
-      type: ContentType.H3,
-      contentValue: 'h3見出し',
-    });
-    const h3 = h3BuildStrategy.build(1);
-    expect(h3.getValue()).toBe('h3見出し');
-  });
+  // it('h3 を生成するストラテジー', () => {
+  //   const h3BuildStrategy = new H3BuildStrategy({
+  //     type: ContentType.H3,
+  //     contentValue: 'h3見出し',
+  //   });
+  //   const h3 = h3BuildStrategy.build(1);
+  //   expect(h3.getValue()).toBe('h3見出し');
+  // });
 
-  it('paragraph を生成するストラテジー', () => {
-    const paragraphBuildStrategy = new ParagraphBuildStrategy({
-      type: ContentType.Paragraph,
-      contentValue: 'テキスト',
-    });
-    const p = paragraphBuildStrategy.build(1);
-    expect(p.getValue()).toBe('テキスト');
-  });
+  // it('paragraph を生成するストラテジー', () => {
+  //   const paragraphBuildStrategy = new ParagraphBuildStrategy({
+  //     type: ContentType.Paragraph,
+  //     contentValue: 'テキスト',
+  //   });
+  //   const p = paragraphBuildStrategy.build(1);
+  //   expect(p.getValue()).toBe('テキスト');
+  // });
 
-  it('image を生成するストラテジー', () => {
-    const imageBuildStrategy = new ImageBuildStrategy({
-      type: ContentType.Image,
-      path: 'path/to/image',
-    });
-    const image = imageBuildStrategy.build(1);
-    expect(image.getPath()).toBe('path/to/image');
-  });
+  // it('image を生成するストラテジー', () => {
+  //   const imageBuildStrategy = new ImageBuildStrategy({
+  //     type: ContentType.Image,
+  //     path: 'path/to/image',
+  //   });
+  //   const image = imageBuildStrategy.build(1);
+  //   expect(image.getPath()).toBe('path/to/image');
+  // });
 });
--- source/frontend/packages/entities/src/blogPost/blogPostBuilder/contentBuilder.ts
+++ source/frontend/packages/entities/src/blogPost/blogPostBuilder/contentBuilder.ts
@@ -1,9 +1,10 @@
-import type { ContentType } from '../postContents/content';
-import { createContent, type Content } from '../postContents/content';
+import {
+  ContentType,
+  createContent,
+  type Content,
+} from '../postContents/content';
 import type { Heading } from '../postContents/heading';
-import { createH2, createH3 } from '../postContents/heading';
-import { ImageContent } from '../postContents/image';
-import { Paragraph } from '../postContents/paragraph';
+import { createH2 } from '../postContents/heading';
 
 export class ContentBuilder {
   private type: ContentType;
@@ -23,20 +24,48 @@ type old__ContentInput = {
   type: ContentType;
   contentValue: string;
 };
-type ContentInput = HeadingInput | ParagraphInput | ImageInput;
-type ContentInputBase = { type: ContentType };
-// TODO rename contentValue->text
-type HeadingInput = { contentValue: string } & ContentInputBase;
-type ParagraphInput = { contentValue: string } & ContentInputBase;
-type ImageInput = { path: string } & ContentInputBase;
 
-type ContentBuildStrategy = {
-  build(id: number): Content;
+export class StrategyContext<T extends Content> {
+  private strategy: ContentBuildStrategy<T>;
+
+  constructor(input: ContentInput<T>) {
+    this.strategy = input.buildStrategy();
+  }
+
+  build(id: number): T {
+    return this.strategy.build(id);
+  }
+}
+
+type ContentInput<T extends Content> = {
+  type: ContentType;
+  buildStrategy(): ContentBuildStrategy<T>;
 };
-export class H2BuildStrategy implements ContentBuildStrategy {
+// TODO rename contentValue->text
+export class H2Input implements ContentInput<Heading> {
+  type: ContentType;
+  contentValue: string;
+
+  constructor(contentValue: string) {
+    this.type = ContentType.H2;
+    this.contentValue = contentValue;
+  }
+
+  buildStrategy(): ContentBuildStrategy<Heading> {
+    return new H2BuildStrategy(this);
+  }
+}
+
+// type ParagraphInput = { contentValue: string } & ContentInputBase;
+// type ImageInput = { path: string } & ContentInputBase;
+
+type ContentBuildStrategy<T extends Content> = {
+  build(id: number): T;
+};
+export class H2BuildStrategy implements ContentBuildStrategy<Heading> {
   private contentValue: string;
 
-  constructor(input: HeadingInput) {
+  constructor(input: H2Input) {
     this.contentValue = input.contentValue;
   }
 
@@ -44,36 +73,36 @@ export class H2BuildStrategy implements ContentBuildStrategy {
     return createH2(id, this.contentValue);
   }
 }
-export class H3BuildStrategy implements ContentBuildStrategy {
-  private contentValue: string;
+// export class H3BuildStrategy implements ContentBuildStrategy {
+//   private contentValue: string;
 
-  constructor(input: HeadingInput) {
-    this.contentValue = input.contentValue;
-  }
+//   constructor(input: HeadingInput) {
+//     this.contentValue = input.contentValue;
+//   }
 
-  build(id: number): Heading {
-    return createH3(id, this.contentValue);
-  }
-}
-export class ParagraphBuildStrategy implements ContentBuildStrategy {
-  private contentValue: string;
+//   build(id: number): Heading {
+//     return createH3(id, this.contentValue);
+//   }
+// }
+// export class ParagraphBuildStrategy implements ContentBuildStrategy {
+//   private contentValue: string;
 
-  constructor(input: ParagraphInput) {
-    this.contentValue = input.contentValue;
-  }
+//   constructor(input: ParagraphInput) {
+//     this.contentValue = input.contentValue;
+//   }
 
-  build(id: number): Paragraph {
-    return new Paragraph(id, this.contentValue);
-  }
-}
-export class ImageBuildStrategy implements ContentBuildStrategy {
-  private path: string;
+//   build(id: number): Paragraph {
+//     return new Paragraph(id, this.contentValue);
+//   }
+// }
+// export class ImageBuildStrategy implements ContentBuildStrategy {
+//   private path: string;
 
-  constructor(input: ImageInput) {
-    this.path = input.path;
-  }
+//   constructor(input: ImageInput) {
+//     this.path = input.path;
+//   }
 
-  build(id: number): ImageContent {
-    return new ImageContent(id, this.path);
-  }
-}
+//   build(id: number): ImageContent {
+//     return new ImageContent(id, this.path);
+//   }
+// }
