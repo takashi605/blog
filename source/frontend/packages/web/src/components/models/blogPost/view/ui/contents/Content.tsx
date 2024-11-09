928ef2466e6c63d5054846faeb6d0c6ab65169fa
takashi605 <95337577+takashi605@users.noreply.github.com>
2024-11-09 13:30:13 +0900

    controllers/types.ts 内の型を各コンポーネント内に移動 #131

--- source/frontend/packages/web/src/components/models/blogPost/view/ui/contents/Content.tsx
+++ source/frontend/packages/web/src/components/models/blogPost/view/ui/contents/Content.tsx
@@ -1,8 +1,12 @@
-import type { BlogPostContentProps } from '@/components/models/blogPost/view/controllers/types';
 import H2 from '@/components/models/blogPost/view/ui/contents/elements/H2';
 import H3 from '@/components/models/blogPost/view/ui/contents/elements/H3';
 import Paragraph from '@/components/models/blogPost/view/ui/contents/elements/Paragraph';
 import { memo } from 'react';
+
+export type BlogPostContentProps = {
+  type: string;
+  value: string;
+};
 
 function ContentRenderer({ type, value }: BlogPostContentProps) {
   switch (type) {
