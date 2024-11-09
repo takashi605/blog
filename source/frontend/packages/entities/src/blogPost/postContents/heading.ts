ebdcb1d5692ccfe7244f2aef8b291256aae12b09
takashi605 <95337577+takashi605@users.noreply.github.com>
2024-11-09 10:55:57 +0900

    H2,H3 の type が自動で入るようにした #131

--- source/frontend/packages/entities/src/blogPost/postContents/heading.ts
+++ source/frontend/packages/entities/src/blogPost/postContents/heading.ts
@@ -1,22 +1,21 @@
 import { ContentType, type ContentBase } from './content';
 
 export const createH2 = (id: number, initialText: string): H2 => {
-  return new H2(id, initialText, ContentType.H2);
+  return new H2(id, initialText);
 };
 
 export const createH3 = (id: number, initialText: string): H3 => {
-  return new H3(id, initialText, ContentType.H3);
+  return new H3(id, initialText);
 };
 
 export class H2 implements ContentBase {
   private id: number;
   private text: string;
-  private type: ContentType.H2;
+  private type: ContentType.H2 = ContentType.H2;
 
-  constructor(id: number, text: string, type: ContentType.H2) {
+  constructor(id: number, text: string) {
     this.id = id;
     this.text = text;
-    this.type = type;
   }
 
   getId() {
@@ -32,12 +31,11 @@ export class H2 implements ContentBase {
 export class H3 implements ContentBase {
   private id: number;
   private text: string;
-  private type: ContentType.H3;
+  private type: ContentType.H3 = ContentType.H3;
 
-  constructor(id: number, text: string, type: ContentType.H3) {
+  constructor(id: number, text: string) {
     this.id = id;
     this.text = text;
-    this.type = type;
   }
 
   getId() {
