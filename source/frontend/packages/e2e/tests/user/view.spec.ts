a17c0ffc22fe009b791ba14bf52ef200a9ce0e3c
takashi605 <95337577+takashi605@users.noreply.github.com>
2024-11-12 08:06:24 +0900

    DockerImage の build 時に NEXT_PUBLIC 環境変数を渡せるようにした #127

--- source/frontend/packages/e2e/tests/user/view.spec.ts
+++ source/frontend/packages/e2e/tests/user/view.spec.ts
@@ -10,6 +10,11 @@ Given('正常な記事が取得できるページにアクセスする', async (
   await page.goto(`${process.env.TEST_TARGET_URL}/posts/1`);
 });
 
+Then('記事サムネイル が表示される', async ({ page }) => {
+  const thumbnailImage = page.getByRole('img', { name: 'サムネイル画像' });
+  await expect(thumbnailImage).toBeVisible();
+});
+
 Then('記事タイトル が表示される', async ({ page }) => {
   const title = page.locator('h1');
 
@@ -65,11 +70,6 @@ Then('投稿日時 が表示される', async ({ page }) => {
 
 Then('更新日時 が表示される', async ({ page }) => {
   await expect(page.getByText(/更新日:\d{4}\/\d{1,2}\/\d{1,2}/)).toBeVisible();
-});
-
-Then('記事サムネイル が表示される', async ({ page }) => {
-  const thumbnailImage = page.getByRole('img', { name: 'サムネイル画像' });
-  await expect(thumbnailImage).toBeVisible();
 });
 
 Given('投稿日,更新日時が存在しないページにアクセスする', async ({ page }) => {
