31198ede03ee241771bc4a3a8ca16d5be611e76a
takashi605 <95337577+takashi605@users.noreply.github.com>
2024-11-20 21:15:28 +0900

    Presenter を別ファイルに分けた #151

--- source/frontend/packages/web/src/components/models/blogPost/view/controllers/ViewBlogPostController.tsx
+++ source/frontend/packages/web/src/components/models/blogPost/view/controllers/ViewBlogPostController.tsx
@@ -1,12 +1,7 @@
-import Thumbnail from '@/components/models/blogPost/view/controllers/Thumbnail';
-import BlogPostDate from '@/components/models/blogPost/view/ui/BlogPostDate';
-import BlogPostTitle from '@/components/models/blogPost/view/ui/BlogPostTitle';
-import ContentRenderer from '@/components/models/blogPost/view/ui/contents/Content';
-import type { BlogPostDTO } from 'service/src/blogPostRepository/repositoryOutput/blogPostDTO';
 import { ApiBlogPostRepository } from 'shared-interface-adapter/src/repositories/apiBlogPostRepository';
 import { ViewBlogPostUseCase } from '../../../../../usecases/view/viewBlogPost';
-import styles from './viewBlogPostController.module.scss';
 import WithBlogPostErrorForServer from '../../error/WithBlogPostErrorForServer';
+import ViewBlogPostPresenter from '../ui/ViewBlogPostPresenter';
 
 type ViewBlogPostControllerProps = {
   postId: string;
@@ -27,43 +22,3 @@ async function ViewBlogPostController({ postId }: ViewBlogPostControllerProps) {
 }
 
 export default WithBlogPostErrorForServer(ViewBlogPostController);
-
-type ViewBlogPostPresenterProps = {
-  blogPost: BlogPostDTO;
-};
-
-// TODO メモ化する
-export function ViewBlogPostPresenter({
-  blogPost,
-}: ViewBlogPostPresenterProps) {
-  return (
-    <article className={styles.article}>
-      <div className={styles.datesWrapper}>
-        <BlogPostDate label="投稿日" date={blogPost.postDate} />
-        <BlogPostDate label="更新日" date={blogPost.lastUpdateDate} />
-      </div>
-      <div className={styles.blogTitle}>
-        <BlogPostTitle>{blogPost?.title}</BlogPostTitle>
-      </div>
-      <Thumbnail thumbnail={blogPost.thumbnail} />
-      {blogPost?.contents.map((content) => (
-        <div key={content.id} className={generateContentClass(content.type)}>
-          <ContentRenderer content={content} />
-        </div>
-      ))}
-    </article>
-  );
-}
-
-function generateContentClass(type: string): string | undefined {
-  switch (type) {
-    case 'paragraph':
-      return styles.paragraph;
-    case 'h2':
-      return styles.h2;
-    case 'h3':
-      return styles.h3;
-    default:
-      return undefined;
-  }
-}
