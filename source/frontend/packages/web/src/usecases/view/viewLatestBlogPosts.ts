c47a6f51284eb22875b3c25c009fc538d94b9be8
takashi605 <95337577+takashi605@users.noreply.github.com>
2024-11-22 08:51:21 +0900

    entity の変換について、一部箇所を新規関数を使うように変更 #157

--- source/frontend/packages/web/src/usecases/view/viewLatestBlogPosts.ts
+++ source/frontend/packages/web/src/usecases/view/viewLatestBlogPosts.ts
@@ -1,8 +1,6 @@
-import type { BlogPost } from 'entities/src/blogPost';
-import type { BlogPostBuilder } from 'service/src/blogPostBuilder';
-import { createBlogPostBuilder } from 'service/src/blogPostBuilder';
 import type { BlogPostRepository } from 'service/src/blogPostRepository';
 import type { BlogPostDTO } from 'service/src/blogPostRepository/repositoryOutput/blogPostDTO';
+import { fetchedDataToEntity } from 'service/src/blogPostRepository/repositoryOutput/blogPostDTOToEntity';
 import { BlogPostDTOBuilder } from './output/dto';
 
 export class ViewLatestBlogPostsUseCase {
@@ -15,36 +13,11 @@ export class ViewLatestBlogPostsUseCase {
   async execute(): Promise<BlogPostDTO[]> {
     const fetchedData = await this.blogPostRepository.fetchLatests();
     const blogPosts = fetchedData.map((dto) => {
-      return this.fetchedDataToEntity(dto);
+      return fetchedDataToEntity(dto);
     });
     const blogPostDTOs = blogPosts.map((blogPost) => {
       return new BlogPostDTOBuilder(blogPost).build();
     });
     return blogPostDTOs;
-  }
-
-  fetchedDataToEntity(dto: BlogPostDTO): BlogPost {
-    const entityBuilder = createBlogPostBuilder()
-      .setId(dto.id)
-      .setPostTitle(dto.title)
-      .setThumbnail(dto.thumbnail.path)
-      .setPostDate(dto.postDate)
-      .setLastUpdateDate(dto.lastUpdateDate);
-
-    this.fetchedContentToEntity(dto, entityBuilder);
-    return entityBuilder.build();
-  }
-  fetchedContentToEntity(dto: BlogPostDTO, entityBuilder: BlogPostBuilder) {
-    dto.contents.forEach((content) => {
-      if (content.type === 'h2') {
-        entityBuilder.addH2(content.id, content.text);
-      } else if (content.type === 'h3') {
-        entityBuilder.addH3(content.id, content.text);
-      } else if (content.type === 'paragraph') {
-        entityBuilder.addParagraph(content.id, content.text);
-      } else if (content.type === 'image') {
-        entityBuilder.addImage(content.id, content.path);
-      }
-    });
   }
 }
