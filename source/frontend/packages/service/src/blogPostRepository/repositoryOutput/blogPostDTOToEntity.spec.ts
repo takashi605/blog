4073babc372bd0c1c10e866c60d5f68ac60f8f85
takashi605 <95337577+takashi605@users.noreply.github.com>
2024-11-22 08:50:06 +0900

    fetch されたデータを entity に変換する関数を実装 #157

---
+++ source/frontend/packages/service/src/blogPostRepository/repositoryOutput/blogPostDTOToEntity.spec.ts
@@ -0,0 +1,31 @@
+import { ContentType } from 'entities/src/blogPost/postContents/content';
+import { createUUIDv4 } from '../../utils/uuid';
+import type { BlogPostDTO } from './blogPostDTO';
+import { fetchedDataToEntity } from './blogPostDTOToEntity';
+
+describe('blogPostDTOToEntity', () => {
+  it('DTO からエンティティに変換する', () => {
+    const id = createUUIDv4();
+    const dto: BlogPostDTO = {
+      id,
+      title: 'title',
+      postDate: '2021-01-01',
+      lastUpdateDate: '2021-01-02',
+      thumbnail: {
+        path: 'path/to/thumbnail',
+      },
+      contents: [
+        { id: 1, type: ContentType.H2, text: 'h2 text' },
+        { id: 2, type: ContentType.H3, text: 'h3 text' },
+      ],
+    };
+    const entity = fetchedDataToEntity(dto);
+
+    expect(entity).toHaveProperty('id', id);
+    expect(entity).toHaveProperty('title', 'title');
+    expect(entity).toHaveProperty('thumbnail');
+    expect(entity).toHaveProperty('postDate');
+    expect(entity).toHaveProperty('lastUpdateDate');
+    expect(entity).toHaveProperty('contents');
+  });
+});
