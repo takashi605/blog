baefd48232d68a91d178a96c7e333320ea9f5ae5
takashi605 <95337577+takashi605@users.noreply.github.com>
2024-11-23 08:42:04 +0900

    新着記事一覧を取得する Controller を作成 #162

---
+++ source/frontend/packages/web/src/controllers/blogPost/view/viewLatestBlogPostsController.spec.tsx
@@ -0,0 +1,43 @@
+import type { ReactElement } from 'react';
+import type { BlogPostDTO } from 'service/src/blogPostRepository/repositoryOutput/blogPostDTO';
+import { setupMockApiForServer } from 'shared-interface-adapter/src/apiMocks/serverForNode';
+import ViewLatestBlogPostsController from './ViewLatestBlogPostsController';
+
+const mockApiForServer = setupMockApiForServer(
+  process.env.NEXT_PUBLIC_API_URL!,
+);
+beforeAll(() => {
+  mockApiForServer.listen();
+});
+afterEach(async () => {
+  mockApiForServer.resetHandlers();
+});
+afterAll(() => {
+  mockApiForServer.close();
+});
+
+describe('viewLatestBlogPostsController', () => {
+  it('レンダリングするコンポーネントに記事データの DTO 配列が渡されている', async () => {
+    const { props } = (await ViewLatestBlogPostsController()) as ReactElement;
+    const dtoList = props.blogPosts as BlogPostDTO[];
+    expect(dtoList).toBeDefined();
+    expect(dtoList.length).toBeGreaterThan(0);
+    dtoList.forEach((dto) => {
+      expect(dto.id).toBeDefined();
+      expect(dto.title).toBeDefined();
+      expect(dto.postDate).toBeDefined();
+      expect(dto.lastUpdateDate).toBeDefined();
+      expect(dto.thumbnail.path).toBeDefined();
+      dto.contents.forEach((content) => {
+        expect(content.id).toBeDefined();
+        expect(content.type).toBeDefined();
+
+        if (content.type === 'image') {
+          expect(content.path).toBeDefined();
+        } else {
+          expect(content.text).toBeDefined();
+        }
+      });
+    });
+  });
+});
