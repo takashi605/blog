2f29709122a3fc4555adb85062bb4e9533285819
takashi605 <95337577+takashi605@users.noreply.github.com>
2024-11-23 09:51:25 +0900

    e2e テストが通るようにした #164

--- source/frontend/packages/web/src/controllers/blogPost/view/ViewLatestBlogPostsPresenter.tsx
+++ source/frontend/packages/web/src/controllers/blogPost/view/ViewLatestBlogPostsPresenter.tsx
@@ -10,6 +10,15 @@ function ViewLatestBlogPostsPresenter({
   return (
     <div>
       <h2>新着記事一覧</h2>
+      <ul>
+        {blogPosts.map((blogPost) => (
+          <li key={blogPost.id}>
+            <h3>{blogPost.title}</h3>
+            <p>投稿日:{blogPost.postDate}</p>
+            <img src={blogPost.thumbnail.path} alt="サムネイル画像" />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
