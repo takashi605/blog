a83178b84d7c5569561d532fc41c66578db8782a
takashi605 <95337577+takashi605@users.noreply.github.com>
2024-11-27 20:37:29 +0900

    不要なファイルを削除 #174

--- source/frontend/packages/web/src/app/page.tsx
+++ source/frontend/packages/web/src/app/page.tsx
@@ -1,62 +1,7 @@
-'use client';
-import Button from '@/components/elements/Button/Button';
-import NumberInput from '@/components/elements/Input/NumberInput';
-import { useInputState } from '@/components/elements/Input/numberInputHooks';
-import { sum } from '@/utils/functions';
-import { useCallback, useState } from 'react';
-
 export default function Home() {
-  const {
-    inputValue: firstNum,
-    setInputValue: setFirstNum,
-    handleChange: handleChangeFirstNum,
-  } = useInputState('');
-  const {
-    inputValue: secondNum,
-    setInputValue: setSecondNum,
-    handleChange: handleChangeSecondNum,
-  } = useInputState('');
-
-  const [result, setResult] = useState('');
-  const handleClickSum = useCallback(() => {
-    const n1 = Number(firstNum);
-    const n2 = Number(secondNum);
-    setResult(sum(n1, n2).toString());
-  }, [firstNum, secondNum]);
-
-  const handleClickFiveSix = useCallback(async () => {
-    const resp = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/fivesix`, {
-      method: 'GET',
-      mode: 'cors',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-    const { num1, num2 } = await resp.json();
-    setFirstNum(num1);
-    setSecondNum(num2);
-  }, [setFirstNum, setSecondNum]);
-
   return (
     <main>
-      <h2>計算機</h2>
-      <Button onClick={handleClickFiveSix} name="fivesix">
-        5・6
-      </Button>
-      <NumberInput
-        name="num1"
-        value={firstNum}
-        onChange={handleChangeFirstNum}
-      />
-      <NumberInput
-        name="num2"
-        value={secondNum}
-        onChange={handleChangeSecondNum}
-      />
-      <Button onClick={handleClickSum} name="calc">
-        計算
-      </Button>
-      結果: <span>{result}</span>
+      <h2>トップページ</h2>
     </main>
   );
 }
--- source/frontend/packages/web/src/components/elements/Input/types.ts
+++
@@ -1,5 +0,0 @@
-export type InputProps = {
-  value: string;
-  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  name?: string;
-};
