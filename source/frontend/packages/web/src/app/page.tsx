0e440c3ebae97f5a996d6822731c95bba08770b7
takashi605 <95337577+takashi605@users.noreply.github.com>
2024-11-30 12:48:24 +0900

    トップページのフェッチ処理をレンダリング時に行うように変更 #128

--- source/frontend/packages/web/src/app/page.tsx
+++ source/frontend/packages/web/src/app/page.tsx
@@ -1,5 +1,10 @@
 import ViewTopTechPickController from '../controllers/blogPost/viewTopTechPick/ViewTopTechPickController';
 
+// ビルド時ではなく、リクエスト時にレンダリングを行う
+// こうしないと MSW によるモックサーバーが機能しない
+// TODO: MSW の使用をやめたら、ビルド時にレンダリングするように変更する
+export const dynamic = 'force-dynamic';
+
 export default async function Home() {
   return (
     <main>
