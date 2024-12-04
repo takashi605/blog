c56d27aa5322dfafe7e0fba9ac316041cff73adc
takashi605 <95337577+takashi605@users.noreply.github.com>
2024-12-04 20:54:49 +0900

    ひとまずクラスを定義 #192

--- source/frontend/packages/web/src/controllers/blogPost/viewPickUpPosts/ViewPickUpPostsPresenter.tsx
+++ source/frontend/packages/web/src/controllers/blogPost/viewPickUpPosts/ViewPickUpPostsPresenter.tsx
@@ -1,5 +1,6 @@
 import type { BlogPostDTO } from 'service/src/blogPostService/dto/blogPostDTO';
 import Thumbnail from '../ui/Thumbnail';
+import styles from './viewPickUpPostsPresenter.module.scss';
 
 type ViewPickUpPostsPresenterProps = {
   blogPostsDTO: BlogPostDTO[];
@@ -9,19 +10,21 @@ function ViewPickUpPostsPresenter({
   blogPostsDTO,
 }: ViewPickUpPostsPresenterProps) {
   return (
-    <div>
-      <div>
-        <span>PICK UP!!</span>
-        <h2>注目記事</h2>
-        <div>
+    <div className={styles.flexContainer}>
+      <div className={styles.flexItem}>
+        <span className={styles.sectionCatchPhrases}>PICK UP!!</span>
+        <h2 className={styles.sectionTitle}>注目記事</h2>
+        <div className={styles.sectionDescription}>
           <p>幅広い人におすすめの記事です。</p>
           <p>このブログの雰囲気を掴んでいただけると思います。</p>
         </div>
       </div>
       {blogPostsDTO.map((blogPostDTO) => (
-        <div key={blogPostDTO.id}>
-          <h3>{blogPostDTO.title}</h3>
-          <Thumbnail thumbnail={blogPostDTO.thumbnail} />
+        <div className={styles.flexItem} key={blogPostDTO.id}>
+          <div className={styles.thumbnail}>
+            <Thumbnail thumbnail={blogPostDTO.thumbnail} />
+          </div>
+          <h3 className={styles.postTitle}>{blogPostDTO.title}</h3>
         </div>
       ))}
     </div>
