91588b8d0d7d23c38ca7456ee520535748a79d67
takashi605 <95337577+takashi605@users.noreply.github.com>
2024-12-08 10:24:26 +0900

    人気記事セレクターのテストを書いた #200

---
+++ source/frontend/packages/service/src/blogPostService/businessLogic/popularPostSelector.spec.ts
@@ -0,0 +1,65 @@
+import type { BlogPost } from 'entities/src/blogPost';
+import { ContentType } from 'entities/src/blogPost/postContents/content';
+import { createUUIDv4 } from '../../utils/uuid';
+import type { BlogPostDTO } from '../dto/blogPostDTO';
+import type { BlogPostRepository } from '../repository/blogPostRepository';
+import { mockBlogPostRepository } from '../../testUtils/blogPostRepositoryMock';
+
+describe('popularPostSelector', () => {
+  it('人気記事に設定されている記事のエンティティデータを取得できる', async () => {
+    const expectedPopularPost: BlogPostDTO[] = buildPopularPostDTO();
+    const fetchPopularPosts = jest.fn().mockReturnValue(expectedPopularPost);
+    const mockRepository: BlogPostRepository = {
+      ...mockBlogPostRepository,
+      fetchPopularPosts,
+    };
+
+    const popularPostSelector = new PopularPostSelector(mockRepository);
+    const popularPosts: BlogPost[] =
+      await popularPostSelector.getPopularPosts(3);
+
+    expect(fetchPopularPosts).toHaveBeenCalled();
+    expect(popularPosts).toHaveLength(3);
+
+    popularPosts.forEach((popularPost) => {
+      expect(popularPost.getId()).toBeDefined();
+      expect(popularPost.getTitleText()).toBeDefined();
+      expect(popularPost.getPostDate()).toBeDefined();
+      expect(popularPost.getLastUpdateDate()).toBeDefined();
+      expect(popularPost.getThumbnail()).toBeDefined();
+      expect(popularPost.getContents()).toBeDefined();
+    });
+  });
+});
+
+function buildPopularPostDTO(): BlogPostDTO[] {
+  const baseMockData: BlogPostDTO = {
+    id: createUUIDv4(),
+    title: '記事タイトル',
+    postDate: '2021-01-01',
+    lastUpdateDate: '2021-01-02',
+    thumbnail: { path: 'path/to/thumbnail' },
+    contents: [
+      { id: createUUIDv4(), type: ContentType.H2, text: 'h2見出し1' },
+      { id: createUUIDv4(), type: ContentType.H3, text: 'h3見出し1' },
+      { id: createUUIDv4(), type: ContentType.Paragraph, text: '段落1' },
+    ],
+  };
+  return [
+    {
+      ...baseMockData,
+      id: createUUIDv4(),
+      title: '記事タイトル1',
+    },
+    {
+      ...baseMockData,
+      id: createUUIDv4(),
+      title: '記事タイトル2',
+    },
+    {
+      ...baseMockData,
+      id: createUUIDv4(),
+      title: '記事タイトル3',
+    },
+  ];
+}
