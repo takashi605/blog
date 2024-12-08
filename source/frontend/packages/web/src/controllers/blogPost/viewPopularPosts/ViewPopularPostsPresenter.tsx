3ce0bf645d72d89511cd62495e9a3125d5772f82
takashi605 <95337577+takashi605@users.noreply.github.com>
2024-12-08 11:12:30 +0900

    人気記事を3件分表示するコンポーネントを作成 #202

---
+++ source/frontend/packages/web/src/controllers/blogPost/viewPopularPosts/ViewPopularPostsPresenter.tsx
@@ -0,0 +1,34 @@
+import type { BlogPostDTO } from 'service/src/blogPostService/dto/blogPostDTO';
+import Thumbnail from '../ui/Thumbnail';
+import styles from './viewPopularPostsPresenter.module.scss';
+
+type ViewPopularPostsPresenterProps = {
+  blogPostsDTO: BlogPostDTO[];
+};
+
+function ViewPopularPostsPresenter({
+  blogPostsDTO,
+}: ViewPopularPostsPresenterProps) {
+  return (
+    <div className={styles.flexContainer}>
+      {/* <div className={styles.flexItem}>
+        <span className={styles.sectionCatchPhrases}>PICK UP!!</span>
+        <h2 className={styles.sectionTitle}>注目記事</h2>
+        <div className={styles.sectionDescription}>
+          <p>幅広い人におすすめの記事です。</p>
+          <p>このブログの雰囲気を掴んでいただけると思います。</p>
+        </div>
+      </div> */}
+      {blogPostsDTO.map((blogPostDTO) => (
+        <div className={styles.flexItem} key={blogPostDTO.id}>
+          <div className={styles.thumbnail}>
+            <Thumbnail thumbnail={blogPostDTO.thumbnail} />
+          </div>
+          <h3 className={styles.postTitle}>{blogPostDTO.title}</h3>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default ViewPopularPostsPresenter;
