ce47447111a61f3d312537239277850944c1dfb9
takashi605 <95337577+takashi605@users.noreply.github.com>
2024-12-08 10:09:25 +0900

    受入テストとなる e2e テストを記述 #198

---
+++ source/frontend/packages/e2e/tests/user/viewPopular.spec.ts
@@ -0,0 +1,33 @@
+import { Before, Given, Then } from '@cucumber/cucumber';
+import type { Page } from '@playwright/test';
+import { chromium, expect } from '@playwright/test';
+
+let page: Page;
+
+Before(async () => {
+  const browser = await chromium.launch({ headless: true });
+  const context = await browser.newContext();
+  page = await context.newPage();
+});
+
+Given('トップページにアクセスして人気記事を閲覧する', async () => {
+  if (!process.env.TEST_TARGET_URL) {
+    throw new Error('TEST_TARGET_URL 環境変数が設定されていません');
+  }
+  await page.goto(`${process.env.TEST_TARGET_URL}`);
+});
+Then('各人気記事のサムネイル画像が3件分表示されている', async () => {
+  const popularSection = getPopularSection(page);
+  const thumbnailImages = popularSection.locator('img');
+  expect(await thumbnailImages.count()).toBe(3);
+});
+Then('各人気記事の記事タイトルが3件分表示されている', async () => {
+  const popularSection = getPopularSection(page);
+  const titles = popularSection.locator('h3');
+  expect(await titles.count()).toBe(3);
+});
+
+function getPopularSection(page: Page) {
+  const popularSectionTitle = page.locator('h2', { hasText: '人気記事' });
+  return page.locator('section', { has: popularSectionTitle });
+}
