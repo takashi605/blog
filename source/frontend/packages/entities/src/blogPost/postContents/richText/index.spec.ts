d7f8c6634030378e9ecb8df93dea233c85d39bea
takashi605 <95337577+takashi605@users.noreply.github.com>
2024-12-14 13:37:43 +0900

    text の構造化を進めるための修正 #209

--- source/frontend/packages/entities/src/blogPost/postContents/richText/index.spec.ts
+++ source/frontend/packages/entities/src/blogPost/postContents/richText/index.spec.ts
@@ -1,8 +1,8 @@
-import { RichText } from '.';
+import { RichText, RichTextPart } from '.';
 
 describe('richText', () => {
   it('単純なテキスト構造を返却する', () => {
-    const richText = new RichText('テストテキスト');
+    const richText = new RichText(new RichTextPart('テストテキスト'));
     expect(richText.getText()).toEqual('テストテキスト');
   });
 });
--- source/frontend/packages/entities/src/blogPost/postContents/richText/index.ts
+++ source/frontend/packages/entities/src/blogPost/postContents/richText/index.ts
@@ -1,4 +1,16 @@
 export class RichText {
+  private text: RichTextPart;
+
+  constructor(text: RichTextPart) {
+    this.text = text;
+  }
+
+  getText() {
+    return this.text.getText();
+  }
+}
+
+export class RichTextPart {
   private text: string;
 
   constructor(text: string) {
