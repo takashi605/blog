756a846e9eabdf7119ae7ad8a3f2a2034c21f4a2
takashi605 <95337577+takashi605@users.noreply.github.com>
2024-12-16 18:38:57 +0900

    スキーマの定義を別ファイルに移した #210

--- source/frontend/packages/shared-interface-adapter/src/repositories/apiBlogPostRepository/index.ts
+++ source/frontend/packages/shared-interface-adapter/src/repositories/apiBlogPostRepository/index.ts
@@ -1,54 +1,11 @@
 import type { BlogPost } from 'entities/src/blogPost';
-import { ContentType } from 'entities/src/blogPost/postContents/content';
 import { ImageContent } from 'entities/src/blogPost/postContents/image';
 import { Paragraph } from 'entities/src/blogPost/postContents/paragraph';
 import type { BlogPostDTO } from 'service/src/blogPostService/dto/blogPostDTO';
 import type { BlogPostRepository } from 'service/src/blogPostService/repository/blogPostRepository';
 import { z } from 'zod';
 import { HttpError } from '../../error/httpError';
-
-const blogPostContentSchema = z.discriminatedUnion('type', [
-  z.object({
-    type: z.literal(ContentType.H2),
-    id: z.string(),
-    text: z.string(),
-  }),
-  z.object({
-    type: z.literal(ContentType.H3),
-    id: z.string(),
-    text: z.string(),
-  }),
-  z.object({
-    type: z.literal(ContentType.Paragraph),
-    id: z.string(),
-    text: z.array(
-      z.object({
-        text: z.string(),
-        styles: z.optional(
-          z.object({
-            bold: z.boolean(),
-          }),
-        ),
-      }),
-    ),
-  }),
-  z.object({
-    type: z.literal(ContentType.Image),
-    id: z.string(),
-    path: z.string(),
-  }),
-]);
-
-export const blogPostResponseSchema: z.ZodType<BlogPostDTO> = z.object({
-  id: z.string(),
-  title: z.string(),
-  thumbnail: z.object({
-    path: z.string(),
-  }),
-  postDate: z.string(),
-  lastUpdateDate: z.string(),
-  contents: z.array(blogPostContentSchema),
-});
+import { blogPostResponseSchema } from './jsonMapper/blogPostSchema';
 
 export class ApiBlogPostRepository implements BlogPostRepository {
   private baseUrl: string;
