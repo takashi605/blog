f12d389abb5946c8225de5ec6048d6ea9437c80c
takashi605 <95337577+takashi605@users.noreply.github.com>
2024-12-21 09:36:17 +0900

    ISO 変換関数をリファクタリング #221

--- source/frontend/packages/service/src/utils/date.ts
+++ source/frontend/packages/service/src/utils/date.ts
@@ -9,6 +9,46 @@ export const formatDate2DigitString = (date: Date): string => {
 
 // 参考記事：https://zenn.dev/shuh/articles/javascript-iso8601
 export function toISOStringWithTimezone(date: Date): string {
+  if (isUTC()) {
+    return ISOStringForUTC();
+  }
+
+  return ISOStringForNoneUTC();
+
+  // 以下ヘルパー関数
+  function ISOStringForUTC() {
+    const tzSign = 'Z';
+    return `${localDate()}T${localTime()}${tzSign}`;
+  }
+
+  function ISOStringForNoneUTC() {
+    const tzSign = diffFromUtc() < 0 ? '+' : '-';
+    const tzHour = zeroPadding((Math.abs(diffFromUtc()) / 60).toString());
+    const tzMinute = zeroPadding((Math.abs(diffFromUtc()) % 60).toString());
+
+    return `${localDate()}T${localTime()}${tzSign}${tzHour}:${tzMinute}`;
+  }
+
+  function isUTC() {
+    return diffFromUtc() === 0;
+  }
+
+  function localDate() {
+    const { year, month, day } = zeroPaddingDateParts(date);
+
+    return `${year}-${month}-${day}`;
+  }
+  function localTime() {
+    const { hour, minute, second } = zeroPaddingDateParts(date);
+    return `${hour}:${minute}:${second}`;
+  }
+
+  function diffFromUtc() {
+    return date.getTimezoneOffset();
+  }
+}
+
+function zeroPaddingDateParts(date: Date) {
   const year = date.getFullYear().toString();
   const month = zeroPadding((date.getMonth() + 1).toString());
   const day = zeroPadding(date.getDate().toString());
@@ -17,23 +57,14 @@ export function toISOStringWithTimezone(date: Date): string {
   const minute = zeroPadding(date.getMinutes().toString());
   const second = zeroPadding(date.getSeconds().toString());
 
-  const localDate = `${year}-${month}-${day}`;
-  const localTime = `${hour}:${minute}:${second}`;
-
-  const diffFromUtc = date.getTimezoneOffset();
-
-  // UTCだった場合
-  if (diffFromUtc === 0) {
-    const tzSign = 'Z';
-    return `${localDate}T${localTime}${tzSign}`;
-  }
-
-  // UTCではない場合
-  const tzSign = diffFromUtc < 0 ? '+' : '-';
-  const tzHour = zeroPadding((Math.abs(diffFromUtc) / 60).toString());
-  const tzMinute = zeroPadding((Math.abs(diffFromUtc) % 60).toString());
-
-  return `${localDate}T${localTime}${tzSign}${tzHour}:${tzMinute}`;
+  return {
+    year,
+    month,
+    day,
+    hour,
+    minute,
+    second,
+  };
 }
 
 function zeroPadding(s: string): string {
