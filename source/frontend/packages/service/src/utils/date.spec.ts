502ba0aa8e50329a55518bc4e886d2d293bda032
takashi605 <95337577+takashi605@users.noreply.github.com>
2024-12-21 11:10:18 +0900

    フォーマットをかけた #221

--- source/frontend/packages/service/src/utils/date.spec.ts
+++ source/frontend/packages/service/src/utils/date.spec.ts
@@ -1,5 +1,5 @@
-import { formatDate2DigitString, toISOStringWithTimezone } from './date';
 import timezoneMock from 'timezone-mock';
+import { formatDate2DigitString, toISOStringWithTimezone } from './date';
 
 describe('日付関連関数', () => {
   it('日付を 2024/01/03 の形式に変換する', () => {
