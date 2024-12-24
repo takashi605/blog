5c6d3e7d6de079f5505f9ccf92d597874d90447c
takashi605 <95337577+takashi605@users.noreply.github.com>
2024-12-24 16:33:49 +0900

    contentsDTO を扱う Provider を作成 #234

--- source/frontend/packages/blog-admin/src/controllers/blogPost/create/CreateBlogPostForm.tsx
+++ source/frontend/packages/blog-admin/src/controllers/blogPost/create/CreateBlogPostForm.tsx
@@ -1,16 +1,20 @@
 'use client';
+import type { Dispatch, SetStateAction } from 'react';
+import { createContext, useState } from 'react';
 import { FormProvider, useForm } from 'react-hook-form';
+import type { ContentDTO } from 'service/src/blogPostService/dto/contentDTO';
 import BlogPostEditor from './blogPostEditor/BlogPostEditor';
 import type { CreateBlogPostFormData } from './formSchema';
 
 function CreateBlogPostForm() {
   const form = useForm<CreateBlogPostFormData>();
+  const [contentsDTO, setContentsDTO] = useState<ContentDTO[]>([]);
 
   const { register, handleSubmit } = form;
 
   // const router = useRouter();
 
-  const onSubmit = async () => {
+  const onSubmit = () => {
     const blogPostTitle = form.getValues('title');
     console.log(blogPostTitle);
     // await createBlogPostAction(formData);
@@ -22,16 +26,25 @@ function CreateBlogPostForm() {
       <BlogPostEditor />
 
       <FormProvider {...form}>
-        <form role="form" onSubmit={handleSubmit(onSubmit)}>
-          <label htmlFor="title">タイトル</label>
+        <contentsDTOContext.Provider value={contentsDTO}>
+          <contentsDTOSetterContext.Provider value={setContentsDTO}>
+            <form role="form" onSubmit={handleSubmit(onSubmit)}>
+              <label htmlFor="title">タイトル</label>
 
-          <input id="title" {...register('title')} />
+              <input id="title" {...register('title')} />
 
-          <button type="submit">投稿</button>
-        </form>
+              <button type="submit">投稿</button>
+            </form>
+          </contentsDTOSetterContext.Provider>
+        </contentsDTOContext.Provider>
       </FormProvider>
     </>
   );
 }
 
 export default CreateBlogPostForm;
+
+const contentsDTOContext = createContext<ContentDTO[]>([]);
+const contentsDTOSetterContext = createContext<Dispatch<
+  SetStateAction<ContentDTO[]>
+> | null>(null);
