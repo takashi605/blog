7ca47c713b34809fa043fc35f3b4fb7e95df0794
takashi605 <95337577+takashi605@users.noreply.github.com>
2024-12-24 18:51:37 +0900

    ユースケースを実行できるようにした #236

--- source/frontend/packages/blog-admin/src/controllers/blogPost/create/CreateBlogPostForm.tsx
+++ source/frontend/packages/blog-admin/src/controllers/blogPost/create/CreateBlogPostForm.tsx
@@ -4,7 +4,11 @@ import type { Dispatch, SetStateAction } from 'react';
 import { createContext, useState } from 'react';
 import { FormProvider, useForm } from 'react-hook-form';
 import type { ContentDTO } from 'service/src/blogPostService/dto/contentDTO';
-import type { BlogPostDTOForCreate } from '../../../usecases/create/createBlogPost';
+import { ApiBlogPostRepository } from 'shared-interface-adapter/src/repositories/apiBlogPostRepository';
+import {
+  CreateBlogPostUseCase,
+  type BlogPostDTOForCreate,
+} from '../../../usecases/create/createBlogPost';
 import BlogPostEditor from './blogPostEditor/BlogPostEditor';
 import { typedBlogPostWithoutContentsToDTO } from './helper/typedBlogPostToDTO';
 
@@ -20,14 +24,23 @@ function CreateBlogPostForm() {
 
   const router = useRouter();
 
-  const onSubmit = () => {
+  const onSubmit = async () => {
     const formValues = form.getValues();
     const blogPostDTO: BlogPostDTOForCreate = {
       ...typedBlogPostWithoutContentsToDTO(formValues),
       contents: contentsDTO,
     };
-    console.log(blogPostDTO);
-    // await createBlogPostAction(formData);
+    if (!process.env.NEXT_PUBLIC_API_URL) {
+      throw new Error('API の URL が設定されていません');
+    }
+    const repository = new ApiBlogPostRepository(
+      process.env.NEXT_PUBLIC_API_URL,
+    );
+    const createBlogPostUseCase = new CreateBlogPostUseCase(
+      blogPostDTO,
+      repository,
+    );
+    await createBlogPostUseCase.execute();
     router.push('/posts/create/success');
   };
 
