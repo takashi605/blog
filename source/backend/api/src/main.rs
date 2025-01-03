c3bbc56f88566ccc03148c8e87b74d426030497d
takashi605 <95337577+takashi605@users.noreply.github.com>
2025-01-03 19:47:35 +0900

    db モジュールを作って pool の定義を移動 #251

--- source/backend/api/src/main.rs
+++ source/backend/api/src/main.rs
@@ -1,21 +1,11 @@
 mod server;
-
-use sqlx::postgres::PgPoolOptions;
-use std::{env, sync::LazyLock};
+mod db;
 
 use anyhow::Result;
 use server::start_api_server;
 
-// シングルトンとしてのPgPoolの定義
-static POOL: LazyLock<sqlx::Pool<sqlx::Postgres>> = LazyLock::new(|| {
-  let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-  PgPoolOptions::new().max_connections(5).connect_lazy(&database_url).expect("Failed to create pool")
-});
-
 #[actix_web::main]
 async fn main() -> Result<()> {
-  // let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-  // let pool = PgPoolOptions::new().max_connections(5).connect(&database_url).await.unwrap();
   // let posts = sqlx::query_as!(
   //   BlogPost,
   //   "select article_id, title, thumbnail_path, created_post_date, updated_post_date, published_at, created_at, updated_at from blog_posts"
