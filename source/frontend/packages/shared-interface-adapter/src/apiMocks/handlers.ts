ff2cda3e6419b2a8aa8744cfd5c14a30d7351457
takashi605 <95337577+takashi605@users.noreply.github.com>
2025-01-04 18:10:27 +0900

    フォーマットをかけた #254

--- source/frontend/packages/shared-interface-adapter/src/apiMocks/handlers.ts
+++ source/frontend/packages/shared-interface-adapter/src/apiMocks/handlers.ts
@@ -1,6 +1,9 @@
 import type { HttpHandler } from 'msw';
 import { http, HttpResponse } from 'msw';
-import { createBlogPostHandlers, createProtTypeBlogPostHandlers } from './handlers/blogPostHandlers';
+import {
+  createBlogPostHandlers,
+  createProtTypeBlogPostHandlers,
+} from './handlers/blogPostHandlers';
 
 export const createAllHandlers = (baseUrl: string): HttpHandler[] => {
   return [
