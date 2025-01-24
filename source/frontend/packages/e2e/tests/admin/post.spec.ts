01d6aa397c61c8b50cde2ece4ef5e74c35516bec
takashi605 <95337577+takashi605@users.noreply.github.com>
2025-01-24 21:25:35 +0900

    デバッグ用の記述を削除

--- source/frontend/packages/e2e/tests/admin/post.spec.ts
+++ source/frontend/packages/e2e/tests/admin/post.spec.ts
@@ -9,11 +9,6 @@ Given('記事投稿ページにアクセスする', async function () {
   }
   const page = playwrightHelper.getPage();
   await page.goto(`${process.env.ADMIN_URL}/posts/create`);
-
-  console.log(`targetURL: ${process.env.ADMIN_URL}/posts/create`);
-  // TODO デバッグ用の記述なので削除
-  const htmlContent = await page.content();
-  console.log(htmlContent);
 });
 
 Then('リッチテキストエディタが表示されていることを確認する', async function () {
