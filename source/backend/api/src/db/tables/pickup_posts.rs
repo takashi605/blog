db37873a41215391f1d51b2a7da448b8956cff7c
takashi605 <95337577+takashi605@users.noreply.github.com>
2025-03-08 15:27:37 +0900

    実際にテーブルから取得したデータでテストが通るようにした SCRUM-81

--- source/backend/api/src/db/tables/pickup_posts.rs
+++ source/backend/api/src/db/tables/pickup_posts.rs
@@ -11,6 +11,6 @@ pub struct PickUpPosts {
 
 pub async fn fetch_all_pickup_blog_posts() -> Result<Vec<PickUpPosts>> {
   // 古い順に3件取得
-  let post = sqlx::query_as::<_, PickUpPosts>("select id, post_id from pickup_posts order by id desc limit 3").fetch_all(&*POOL).await?;
+  let post = sqlx::query_as::<_, PickUpPosts>("select id, post_id from pickup_posts order by updated_at desc limit 3").fetch_all(&*POOL).await?;
   Ok(post)
 }
