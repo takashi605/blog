248d48bc6b643ddb6905de48ae8eee270afc0453
takashi605 <95337577+takashi605@users.noreply.github.com>
2025-03-12 17:28:47 +0900

    rename struct PickUpPostRecord->PickUpPostRecord SCRUM-88

--- source/backend/api/src/db/tables/pickup_posts.rs
+++ source/backend/api/src/db/tables/pickup_posts.rs
@@ -5,13 +5,13 @@ use uuid::Uuid;
 
 // TODO PickUpPostRecord という名前に変更する
 #[derive(Debug, FromRow)]
-pub struct PickUpPosts {
+pub struct PickUpPostRecord {
   pub id: Uuid,
   pub post_id: Uuid,
 }
 
-pub async fn fetch_all_pickup_blog_posts() -> Result<Vec<PickUpPosts>> {
+pub async fn fetch_all_pickup_blog_posts() -> Result<Vec<PickUpPostRecord>> {
   // 古い順に3件取得
-  let post = sqlx::query_as::<_, PickUpPosts>("select id, post_id from pickup_posts order by updated_at desc limit 3").fetch_all(&*POOL).await?;
+  let post = sqlx::query_as::<_, PickUpPostRecord>("select id, post_id from pickup_posts order by updated_at desc limit 3").fetch_all(&*POOL).await?;
   Ok(post)
 }
