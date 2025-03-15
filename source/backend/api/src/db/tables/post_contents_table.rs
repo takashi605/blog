24cfcaee760a3951f2e987fb127684470af5f4ac
takashi605 <95337577+takashi605@users.noreply.github.com>
2025-03-15 11:45:29 +0900

    HeadingBlockRecord | ImageBlockRecord |ParagraphBlockRecordWithRelationsを扱う enum AnyContentBlockRecord を定義 SCRUM-84

--- source/backend/api/src/db/tables/post_contents_table.rs
+++ source/backend/api/src/db/tables/post_contents_table.rs
@@ -4,6 +4,21 @@ use uuid::Uuid;
 
 use crate::db::pool::POOL;
 
+use super::{heading_blocks_table::HeadingBlockRecord, image_blocks_table::ImageBlockRecord, paragraph_blocks_table::ParagraphBlockRecordWithRelations};
+
+/*
+ * 各レコードの関連をまとめた構造体
+ */
+// PostContentRecord の id を元に取得される、より具体的なコンテンツのレコード
+pub enum AnyContentBlockRecord {
+  HeadingBlockRecord(HeadingBlockRecord),
+  ParagraphBlockRecord(ParagraphBlockRecordWithRelations),
+  ImageBlockRecord(ImageBlockRecord),
+}
+
+/*
+ * DB内の各テーブル構造に紐づく構造体正義
+ */
 #[derive(Debug, FromRow)]
 pub struct PostContentRecord {
   pub id: Uuid,
@@ -12,6 +27,9 @@ pub struct PostContentRecord {
   pub sort_order: i32,
 }
 
+/*
+ * テーブル構造体の操作を便利にするための構造体・関数
+ */
 // content_type が何を表しているかを enum で表現
 // content_type に match 式を使う時はこの enum を使う
 pub enum PostContentType {
@@ -33,6 +51,9 @@ impl TryFrom<String> for PostContentType {
   }
 }
 
+/*
+ * データベース操作関数
+ */
 pub async fn fetch_post_contents_by_post_id(post_id: Uuid) -> Result<Vec<PostContentRecord>> {
   let contents =
     sqlx::query_as::<_, PostContentRecord>("select id, post_id, content_type, sort_order from post_contents where post_id = $1").bind(post_id).fetch_all(&*POOL).await?;
