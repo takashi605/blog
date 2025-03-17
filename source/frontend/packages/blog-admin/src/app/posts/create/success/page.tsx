53379328230b6f61f26415003ab4e7ebe609684f
takashi605 <95337577+takashi605@users.noreply.github.com>
2025-03-17 17:06:29 +0900

    記事投稿後に環境変数で設定されたドメインに遷移できるようにした SCRUM-107

--- source/frontend/packages/blog-admin/src/app/posts/create/success/page.tsx
+++ source/frontend/packages/blog-admin/src/app/posts/create/success/page.tsx
@@ -1,10 +1,14 @@
 import Link from 'next/link';
 
 function CreateBlogPostSuccessPage() {
+  if (!process.env.NEXT_PUBLIC_WEB_URL) {
+    throw new Error('WEB_URL が設定されていません');
+  }
+  const web_url = process.env.NEXT_PUBLIC_WEB_URL;
   return (
     <div>
       <p>記事を公開しました</p>
-      <Link href="https://example.com">投稿した記事を見る</Link>
+      <Link href={`${web_url}`}>投稿した記事を見る</Link>
     </div>
   );
 }
