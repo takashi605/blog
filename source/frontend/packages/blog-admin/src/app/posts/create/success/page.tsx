e89508f5ebd08af7e02688de86705a6ff5f3d5eb
takashi605 <95337577+takashi605@users.noreply.github.com>
2025-03-17 17:20:44 +0900

    投稿成功ページに投稿したページへのリンクを設置した SCRUM-107

--- source/frontend/packages/blog-admin/src/app/posts/create/success/page.tsx
+++ source/frontend/packages/blog-admin/src/app/posts/create/success/page.tsx
@@ -1,14 +1,26 @@
 import Link from 'next/link';
 
-function CreateBlogPostSuccessPage() {
+function CreateBlogPostSuccessPage({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
   if (!process.env.NEXT_PUBLIC_WEB_URL) {
     throw new Error('WEB_URL が設定されていません');
   }
   const web_url = process.env.NEXT_PUBLIC_WEB_URL;
+
+  const postedPageId = searchParams.id;
+  if (!postedPageId) {
+    throw new Error('id が設定されていません');
+  }
+
+  const postedPageUrl = `${web_url}/posts/${postedPageId}`;
+
   return (
     <div>
       <p>記事を公開しました</p>
-      <Link href={`${web_url}`}>投稿した記事を見る</Link>
+      <Link href={postedPageUrl}>投稿した記事を見る</Link>
     </div>
   );
 }
