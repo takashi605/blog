8b5d972b3026c8005839a9bfa3cb65efeec62559
takashi605 <95337577+takashi605@users.noreply.github.com>
2025-03-17 16:53:52 +0900

    example.com へのリンクを追加 SCRUM-107

--- source/frontend/packages/blog-admin/src/app/posts/create/success/page.tsx
+++ source/frontend/packages/blog-admin/src/app/posts/create/success/page.tsx
@@ -1,5 +1,12 @@
+import Link from 'next/link';
+
 function CreateBlogPostSuccessPage() {
-  return <div>記事を公開しました</div>;
+  return (
+    <div>
+      <p>記事を公開しました</p>
+      <Link href="https://example.com">投稿した記事を見る</Link>
+    </div>
+  );
 }
 
 export default CreateBlogPostSuccessPage;
