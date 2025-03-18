4e6fa55e46e468f1f9c65e767c9fe50ea2b577ad
takashi605 <95337577+takashi605@users.noreply.github.com>
2025-03-18 17:53:40 +0900

    モーダルの開閉ができるようにした SCRUM-100

--- source/frontend/packages/blog-admin/src/controllers/images/list/ImageList.tsx
+++ source/frontend/packages/blog-admin/src/controllers/images/list/ImageList.tsx
@@ -1,8 +1,29 @@
+'use client';
+import { useCallback, useState } from 'react';
+import Modal from 'react-modal';
+
 function ImageList() {
+  const [isOpen, setIsOpen] = useState(false);
+  const openModal = useCallback(() => {
+    setIsOpen(true);
+  }, []);
+  const closeModal = useCallback(() => {
+    setIsOpen(false);
+  }, []);
   return (
     <>
       <h2>画像の管理</h2>
-      <button>画像を追加</button>;
+      <button onClick={openModal}>画像を追加</button>
+      <Modal
+        isOpen={isOpen}
+        onRequestClose={closeModal}
+        className="modal"
+        overlayClassName="overlay"
+      >
+        <button onClick={closeModal} className="modal-close" type="button">
+          閉じる
+        </button>
+      </Modal>
     </>
   );
 }
