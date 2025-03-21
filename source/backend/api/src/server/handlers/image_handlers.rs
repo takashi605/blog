59fb6a9c60c96864882dfcdbe2135932941fb0ee
takashi605 <95337577+takashi605@users.noreply.github.com>
2025-03-21 16:10:23 +0900

    適当なモックレスポンスでテストを通した SCRUM-104

--- source/backend/api/src/server/handlers/image_handlers.rs
+++ source/backend/api/src/server/handlers/image_handlers.rs
@@ -3,7 +3,9 @@ pub mod fetch_images;
 use actix_web::{web, Scope};
 
 pub fn image_scope() -> Scope {
-  web::scope("/images").route("", web::get().to(handle_funcs::get_images))
+  web::scope("/images")
+  .route("", web::get().to(handle_funcs::get_images))
+  .route("", web::post().to(handle_funcs::create_image))
 }
 
 mod handle_funcs {
@@ -15,5 +17,14 @@ mod handle_funcs {
   pub async fn get_images() -> Result<impl Responder, ApiCustomError> {
     let images: Vec<Image> = fetch_images().await?;
     Ok(HttpResponse::Ok().json(images))
+  }
+
+  pub async fn create_image() -> Result<impl Responder, ApiCustomError> {
+    // とりあえず適当な Image を返す
+    let image = Image {
+      id: uuid::Uuid::new_v4(),
+      path: "test-image".to_string(),
+    };
+    Ok(HttpResponse::Ok().json(image))
   }
 }
