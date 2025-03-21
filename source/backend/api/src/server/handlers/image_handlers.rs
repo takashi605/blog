a7ae15e7404ec4fb2708140f2e8b8de9fc7746c6
takashi605 <95337577+takashi605@users.noreply.github.com>
2025-03-21 16:24:52 +0900

    画像を挿入する api を作った SCRUM-104

--- source/backend/api/src/server/handlers/image_handlers.rs
+++ source/backend/api/src/server/handlers/image_handlers.rs
@@ -3,15 +3,13 @@ pub mod fetch_images;
 use actix_web::{web, Scope};
 
 pub fn image_scope() -> Scope {
-  web::scope("/images")
-  .route("", web::get().to(handle_funcs::get_images))
-  .route("", web::post().to(handle_funcs::create_image))
+  web::scope("/images").route("", web::get().to(handle_funcs::get_images)).route("", web::post().to(handle_funcs::create_image))
 }
 
 mod handle_funcs {
   use super::fetch_images::fetch_images;
-  use crate::server::handlers::response::err::ApiCustomError;
-  use actix_web::{HttpResponse, Responder};
+  use crate::{db::tables::images_table::insert_image, server::handlers::response::err::ApiCustomError};
+  use actix_web::{web, HttpResponse, Responder};
   use common::types::api::response::Image;
 
   pub async fn get_images() -> Result<impl Responder, ApiCustomError> {
@@ -19,11 +17,12 @@ mod handle_funcs {
     Ok(HttpResponse::Ok().json(images))
   }
 
-  pub async fn create_image() -> Result<impl Responder, ApiCustomError> {
-    // とりあえず適当な Image を返す
+  pub async fn create_image(image_by_req: web::Json<Image>) -> Result<impl Responder, ApiCustomError> {
+    let image_by_req = image_by_req.into_inner();
+    let image_record = insert_image(image_by_req).await.map_err(|_| ApiCustomError::Other(anyhow::anyhow!("画像の挿入に失敗しました。")))?;
     let image = Image {
-      id: uuid::Uuid::new_v4(),
-      path: "test-image".to_string(),
+      id: image_record.id,
+      path: image_record.file_path,
     };
     Ok(HttpResponse::Ok().json(image))
   }
