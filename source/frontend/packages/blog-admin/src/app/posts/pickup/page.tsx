dbe6997b88aec43b0df7c8c0f4bcadeb7d3452d3
takashi605 <95337577+takashi605@users.noreply.github.com>
2025-03-29 20:41:41 +0900

    リスト表示するコンポーネントを空で作成 SCRUM-137

--- source/frontend/packages/blog-admin/src/app/posts/pickup/page.tsx
+++ source/frontend/packages/blog-admin/src/app/posts/pickup/page.tsx
@@ -1,7 +1,10 @@
+import PickUpPostList from '../../../controllers/blogPost/pickup/PickUpPostList';
+
 export default function PickUpManagementPage() {
   return (
     <div>
       <h2>ピックアップ記事管理</h2>
+      <PickUpPostList />
     </div>
   );
 }
---
+++ source/frontend/packages/blog-admin/src/controllers/blogPost/pickup/PickUpPostList.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PickUpPostList() {
+  return (
+    <div>PickUpPostList</div>
+  )
+}
+
+export default PickUpPostList
