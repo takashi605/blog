73afd7dd1cf93f5e0884abe31508ef99e796094c
takashi605 <95337577+takashi605@users.noreply.github.com>
2025-03-29 17:59:38 +0900

    api リポジトリに selectPickUpPosts を実装 SCRUM-135

--- source/frontend/packages/shared-interface-adapter/src/repositories/apiBlogPostRepository/index.ts
+++ source/frontend/packages/shared-interface-adapter/src/repositories/apiBlogPostRepository/index.ts
@@ -4,7 +4,7 @@ import type { BlogPostRepository } from 'service/src/blogPostService/repository/
 import { z } from 'zod';
 import { HttpError } from '../../error/httpError';
 import { blogPostResponseSchema } from './jsonMapper/blogPostSchema';
-import { blogPostToJson } from './jsonMapper/blogPostToJson';
+import { blogPostsToJson, blogPostToJson } from './jsonMapper/blogPostToJson';
 
 export class ApiBlogPostRepository implements BlogPostRepository {
   private baseUrl: string;
@@ -92,6 +92,25 @@ export class ApiBlogPostRepository implements BlogPostRepository {
       .array(blogPostResponseSchema)
       .parse(await response.json());
 
+    return validatedResponse;
+  }
+
+  async selectPickUpPosts(newPickUpPosts: BlogPost[]): Promise<BlogPostDTO[]> {
+    const body = blogPostsToJson(newPickUpPosts);
+    const response = await fetch(`${this.baseUrl}/blog/posts/pickup`, {
+      method: 'PUT',
+      body,
+      ...this.baseFetchOptions,
+    });
+    if (!response.ok) {
+      const message = await response.text();
+      throw new Error(
+        `ピックアップ記事の更新に失敗しました:\n${message.replace(/\\n/g, '\n').replace(/\\"/g, '"')}`,
+      );
+    }
+    const validatedResponse = z
+      .array(blogPostResponseSchema)
+      .parse(await response.json());
     return validatedResponse;
   }
 
