f9fc2734d924dc74f34ea8a8d0bf2ea3f8e6bd32
takashi605 <95337577+takashi605@users.noreply.github.com>
2025-03-29 21:29:49 +0900

    イメージアップロード用のモーダルを空で設置 SCRUM-138

---
+++ source/frontend/packages/blog-admin/src/controllers/blogPost/pickup/select/PickUpPostSelectModal.tsx
@@ -0,0 +1,59 @@
+'use client';
+import React from 'react';
+import CommonModal from '../../../../components/modal/CommonModal';
+import { useCommonModalContext } from '../../../../components/modal/CommonModalProvider';
+import type { ImageUploadFormValues } from '../../../images/upload/form/ImageUploadFormProvider';
+import { usePickUpPostListContext } from '../list/PickUpPostListProvider';
+
+function PickUpPostSelectModalWithOpenButton() {
+  const { openModal } = useCommonModalContext();
+
+  return (
+    <>
+      <button onClick={openModal}>ピックアップ記事を選択</button>
+      <Modal />
+    </>
+  );
+}
+
+function Modal() {
+  const { updatePickUpPosts } = usePickUpPostListContext();
+  const [isUploadSuccess, setIsUploadSuccess] = React.useState(false);
+  const { closeModal } = useCommonModalContext();
+
+  const onSubmit = async (data: ImageUploadFormValues) => {
+    console.log(data);
+    // if (!process.env.NEXT_PUBLIC_API_URL) {
+    //   throw new Error('API の URL が設定されていません');
+    // }
+    // const blogPostRepository = new ApiBlogPostRepository(
+    //   process.env.NEXT_PUBLIC_API_URL,
+    // );
+    // const selectedPickUpPosts = null;
+    // const selectPickUpPostsUseCase = new SelectPickUpPostsUseCase(
+    //   selectedPickUpPosts,
+    //   blogPostRepository,
+    // );
+    // try {
+    //   const updatedPickUpPosts = await selectPickUpPostsUseCase.execute();
+    //   updatePickUpPosts(updatedPickUpPosts);
+    //   setIsUploadSuccess(true);
+    // } catch {
+    //   alert('ピックアップ記事の更新に失敗しました。ログを確認してください。');
+    // }
+  };
+
+  return (
+    <CommonModal>
+      {/* <ImageUploadFormProvider>
+        <ImageUploadForm onSubmit={onSubmit} />
+        {isUploadSuccess && <p>画像のアップロードに成功しました</p>}
+      </ImageUploadFormProvider> */}
+      <button onClick={closeModal} className="modal-close" type="button">
+        閉じる
+      </button>
+    </CommonModal>
+  );
+}
+
+export default React.memo(PickUpPostSelectModalWithOpenButton);
