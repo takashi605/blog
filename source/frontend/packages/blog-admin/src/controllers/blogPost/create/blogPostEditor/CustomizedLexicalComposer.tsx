a117ac0a604e6b1cba9930dc9bc3ad6fe7fe07fe
takashi605 <95337577+takashi605@users.noreply.github.com>
2025-03-29 14:57:01 +0900

    rename package shared-styles->shared-ui SCRUM-127

--- source/frontend/packages/blog-admin/src/controllers/blogPost/create/blogPostEditor/CustomizedLexicalComposer.tsx
+++ source/frontend/packages/blog-admin/src/controllers/blogPost/create/blogPostEditor/CustomizedLexicalComposer.tsx
@@ -5,7 +5,7 @@ import { LexicalComposer } from '@lexical/react/LexicalComposer';
 import { HorizontalRuleNode } from '@lexical/react/LexicalHorizontalRuleNode';
 import { HeadingNode, QuoteNode } from '@lexical/rich-text';
 import { ParagraphNode, TextNode } from 'lexical';
-import styles from 'shared-styles/src/styles/blogPostViewer.module.scss';
+import styles from 'shared-ui/src/styles/blogPostViewer.module.scss';
 
 const theme = {
   code: 'editor-code',
