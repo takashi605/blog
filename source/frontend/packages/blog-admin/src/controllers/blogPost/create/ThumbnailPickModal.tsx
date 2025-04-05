ee7bd50b94587d3c55273dd1272a181ec62e1ae6
takashi605 <95337577+takashi605@users.noreply.github.com>
2025-04-05 17:00:24 +0900

    ImagePicker の onChange 等を props で渡す形に変更 SCRUM-149

--- source/frontend/packages/blog-admin/src/controllers/blogPost/create/ThumbnailPickModal.tsx
+++ source/frontend/packages/blog-admin/src/controllers/blogPost/create/ThumbnailPickModal.tsx
@@ -1,3 +1,5 @@
+import { useCallback } from 'react';
+import { useFormContext } from 'react-hook-form';
 import CommonModal from '../../../components/modal/CommonModal';
 import CommonModalCloseButton from '../../../components/modal/CommonModalCloseButton';
 import CommonModalOpenButton from '../../../components/modal/CommonModalOpenButton';
@@ -5,12 +7,29 @@ import ImageListProvider from '../../images/list/ImageListProvider';
 import ImagePicker from '../../images/pick/ImagePicker';
 
 function ThumbnailPickModalWithOpenButton() {
+  const { register, setValue } = useFormContext();
+
+  const { onChange, onBlur, name, ref } = register('thumbnail.id');
+
+  const onChangePickHandler = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      onChange(e);
+      setValue('thumbnail.path', e.target.value);
+    },
+    [onChange, setValue],
+  );
+
   return (
     <>
       <CommonModalOpenButton>サムネイル画像を選択</CommonModalOpenButton>
       <CommonModal>
         <ImageListProvider>
-          <ImagePicker />
+          <ImagePicker
+            onChange={onChangePickHandler}
+            onBlur={onBlur}
+            name={name}
+            ref={ref}
+          />
         </ImageListProvider>
         <CommonModalCloseButton>閉じる</CommonModalCloseButton>
       </CommonModal>
--- source/frontend/packages/blog-admin/src/controllers/images/pick/ImagePicker.tsx
+++ source/frontend/packages/blog-admin/src/controllers/images/pick/ImagePicker.tsx
@@ -1,16 +1,16 @@
 'use client';
 import { CldImage } from 'next-cloudinary';
-import { useFormContext } from 'react-hook-form';
 import { useImageList } from '../list/useImageList';
 
-function ImagePicker() {
-  const { getAllImages } = useImageList();
-  const { register, setValue } = useFormContext();
+type ImagePickerProps = {
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onBlur?: (e: React.FocusEvent<HTMLInputElement>) => void;
+  name?: string;
+  ref?: React.Ref<HTMLInputElement>;
+};
 
-  // 各ラジオボタンに共通の register を渡すために上部で定義
-  // 個別に register を実行して渡すと、最後の要素だけが反映されてしまう
-  // これは id が共通のものは上書きする react-hook-form の仕様による挙動
-  const { onChange, onBlur, name, ref } = register('thumbnail.id');
+function ImagePicker({ onChange, onBlur, name, ref }: ImagePickerProps) {
+  const { getAllImages } = useImageList();
 
   return (
     <>
@@ -22,13 +22,10 @@ function ImagePicker() {
               id={image.id}
               type="radio"
               value={image.id}
-              name={name}
-              ref={ref}
-              onChange={(e) => {
-                onChange(e);
-                setValue('thumbnail.path', image.path);
-              }}
-              onBlur={onBlur}
+              onChange={onChange}
+              {...(onBlur && { onBlur })}
+              {...(name && { name })}
+              {...(ref && { ref })}
             />
             <label htmlFor={image.id}>
               <CldImage
