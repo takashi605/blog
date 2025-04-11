f78116fa29255a7ca34762789e66385def0386b1
takashi605 <95337577+takashi605@users.noreply.github.com>
2025-04-11 16:31:23 +0900

    コピーボタンの見た目だけ作成した SCRUM-169

--- source/frontend/packages/web/src/controllers/blogPost/ui/contents/elements/CodeBlock.tsx
+++ source/frontend/packages/web/src/controllers/blogPost/ui/contents/elements/CodeBlock.tsx
@@ -1,6 +1,8 @@
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { vscDarkPlus } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 import type { CodeBlockDTO } from 'service/src/blogPostService/dto/contentDTO';
+import { MdCopyAll } from 'react-icons/md';
+import styles from './codeBlock.module.scss'
 
 type CodeBlockProps = {
   codeBlockData: CodeBlockDTO;
@@ -8,13 +10,18 @@ type CodeBlockProps = {
 
 function CodeBlock({ codeBlockData }: CodeBlockProps) {
   return (
-    <SyntaxHighlighter
-      style={vscDarkPlus}
-      language={codeBlockData.language}
-      PreTag="div"
-    >
-      {codeBlockData.code}
-    </SyntaxHighlighter>
+    <div className={styles.codeBlockWrapper}>
+      <div className={styles.copyIcon}>
+        <MdCopyAll size="28px"/>
+      </div>
+      <SyntaxHighlighter
+        style={vscDarkPlus}
+        language={codeBlockData.language}
+        PreTag="div"
+      >
+        {codeBlockData.code}
+      </SyntaxHighlighter>
+    </div>
   );
 }
 
