eb1e39e923f93903f91f3f39e09ae4602df3aba8
takashi605 <95337577+takashi605@users.noreply.github.com>
2025-04-19 14:51:35 +0900

    ヘッダーにリンクを設置 SCRUM-204

--- source/frontend/packages/web/src/components/topLayout/header/Header.tsx
+++ source/frontend/packages/web/src/components/topLayout/header/Header.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { memo } from 'react';
 import styles from './header.module.scss';
 
@@ -5,7 +6,20 @@ function Header() {
   return (
     <div className={styles.headerWrapper}>
       <header className={styles.header}>
-        <div className={styles.blogTitle}>鉄火ブログ</div>
+        <div className={styles.blogTitle}>
+          <Link href="/" className={`${styles.link} ${styles.blogTitle}`}>
+            鉄火ブログ
+          </Link>
+        </div>
+        <nav className={styles.nav}>
+          <ul>
+            <li>
+              <Link href="/posts/latest" className={styles.link}>
+                新着記事
+              </Link>
+            </li>
+          </ul>
+        </nav>
       </header>
     </div>
   );
