8f47e9e9a906d0be45f40ab41e811c88624ff1ae
takashi605 <95337577+takashi605@users.noreply.github.com>
2025-05-02 17:03:30 +0900

    useExcerpted を削除し、同じ処理の別の関数を使うようにした SCRUM-240

--- source/frontend/packages/service/src/blogPostService/dto/blogPostDTOProcessor/excerptedBlogPostDTO.spec.ts
+++ source/frontend/packages/service/src/blogPostService/dto/blogPostDTOProcessor/excerptedBlogPostDTO.spec.ts
@@ -1,10 +1,26 @@
-import { mockBlogPostDTO } from '../../../mockData/mockBlogPostDTO';
-import { extractFirstParagraphText } from './excerptedBlogPostDTO';
+import {
+  mockBlogPostDTO,
+  mockRichTextDTO,
+} from '../../../mockData/mockBlogPostDTO';
+import {
+  extractFirstParagraph,
+  extractFirstParagraphText,
+} from './excerptedBlogPostDTO';
 
 describe('excerptedBlogPostDTO', () => {
-  it('記事コンテンツの最も上部に位置する段落のテキストを取得する', () => {
-    const dto = mockBlogPostDTO;
-    const result = extractFirstParagraphText(dto);
-    expect(result).toEqual('これはテストテキストです');
+  describe('extractFirstParagraphText 関数', () => {
+    it('記事コンテンツの最も上部に位置する段落のテキストを取得する', () => {
+      const dto = mockBlogPostDTO;
+      const result = extractFirstParagraphText(dto);
+      expect(result).toEqual('これはテストテキストです');
+    });
+  });
+
+  describe('extractFirstParagraph 関数', () => {
+    it('記事コンテンツの最も上部に位置する段落を取得する', () => {
+      const dto = mockBlogPostDTO;
+      const result = extractFirstParagraph(dto);
+      expect(result).toEqual(mockRichTextDTO());
+    });
   });
 });
