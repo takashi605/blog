99c82ae49af0ad04b9fcdfcb96a629eeb3f1f81c
takashi605 <95337577+takashi605@users.noreply.github.com>
2025-05-02 16:29:17 +0900

    各記事のメタデータを生成する準備を整えた SCRUM-240

--- source/frontend/packages/web/src/app/posts/[id]/page.tsx
+++ source/frontend/packages/web/src/app/posts/[id]/page.tsx
@@ -1,3 +1,4 @@
+import type { Metadata, ResolvingMetadata } from 'next';
 import ViewBlogPostController from '../../../controllers/blogPost/viewBlogPost/ViewBlogPostController';
 
 type ViewBlogPostParams = {
@@ -5,6 +6,17 @@ type ViewBlogPostParams = {
     id: string;
   };
 };
+
+export async function generateMetadata(
+  _parent: ResolvingMetadata,
+): Promise<Metadata> {
+  // const post = await getBlogPost(params.id);
+
+  return {
+    title: '記事タイトル',
+    description: '記事の説明文',
+  };
+}
 
 async function ViewBlogPost({ params }: ViewBlogPostParams) {
   const { id: postId } = params;
