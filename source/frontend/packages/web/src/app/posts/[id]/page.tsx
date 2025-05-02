a66c3c6ab48e89dd7a0b1fdf8f01c52ec6340846
takashi605 <95337577+takashi605@users.noreply.github.com>
2025-05-02 16:31:15 +0900

    記事タイトルが動的に切り替わるようにした SCRUM-240

--- source/frontend/packages/web/src/app/posts/[id]/page.tsx
+++ source/frontend/packages/web/src/app/posts/[id]/page.tsx
@@ -1,5 +1,7 @@
 import type { Metadata, ResolvingMetadata } from 'next';
+import { ApiBlogPostRepository } from 'shared-interface-adapter/src/repositories/apiBlogPostRepository';
 import ViewBlogPostController from '../../../controllers/blogPost/viewBlogPost/ViewBlogPostController';
+import { ViewBlogPostUseCase } from '../../../usecases/view/viewBlogPost';
 
 type ViewBlogPostParams = {
   params: {
@@ -8,12 +10,21 @@ type ViewBlogPostParams = {
 };
 
 export async function generateMetadata(
+  { params }: ViewBlogPostParams,
   _parent: ResolvingMetadata,
 ): Promise<Metadata> {
-  // const post = await getBlogPost(params.id);
+  if (!process.env.NEXT_PUBLIC_API_URL) {
+    throw new Error('API URL が設定されていません');
+  }
+  const blogPostRepository = new ApiBlogPostRepository(
+    process.env.NEXT_PUBLIC_API_URL,
+  );
+  const blogPostDTO = await new ViewBlogPostUseCase(blogPostRepository).execute(
+    params.id,
+  );
 
   return {
-    title: '記事タイトル',
+    title: blogPostDTO.title,
     description: '記事の説明文',
   };
 }
