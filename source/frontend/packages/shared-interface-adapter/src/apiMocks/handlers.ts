b11192c07d79f0146480f04d35cc50b52267eafb
takashi605 <95337577+takashi605@users.noreply.github.com>
2025-05-22 19:27:58 +0900

    フォーマットをかけた SCRUM-257

--- source/frontend/packages/shared-interface-adapter/src/apiMocks/handlers.ts
+++ source/frontend/packages/shared-interface-adapter/src/apiMocks/handlers.ts
@@ -3,8 +3,5 @@ import { createBlogPostHandlers } from './handlers/blogPost/blogPostHandlers';
 import { createImageHandlers } from './handlers/image/imageHandler';
 
 export const createAllHandlers = (baseUrl: string): HttpHandler[] => {
-  return [
-    ...createBlogPostHandlers(baseUrl),
-    ...createImageHandlers(baseUrl),
-  ];
+  return [...createBlogPostHandlers(baseUrl), ...createImageHandlers(baseUrl)];
 };
