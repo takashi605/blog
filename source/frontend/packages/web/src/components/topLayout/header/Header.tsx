a922c74521db50f7f382d34e1145797025102112
takashi605 <95337577+takashi605@users.noreply.github.com>
2025-05-22 20:39:53 +0900

    ハンバーガーメニューを SpNav コンポーネントに切り出し SCRUM-282

--- source/frontend/packages/web/src/components/topLayout/header/Header.tsx
+++ source/frontend/packages/web/src/components/topLayout/header/Header.tsx
@@ -1,18 +1,10 @@
 'use client';
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-import { memo, useEffect, useState } from 'react';
+import { memo } from 'react';
 import styles from './header.module.scss';
+import SpNav from './SpNav';
 
 function Header() {
-  const [isOpenSpNav, setIsOpenSpNav] = useState(false);
-
-  /* ルート（pathname）が変わったら必ず閉じる */
-  const pathname = usePathname();
-  useEffect(() => {
-    setIsOpenSpNav(false);
-  }, [pathname]);
-
   return (
     <>
       <header className={styles.header}>
@@ -31,31 +23,8 @@ function Header() {
           </ul>
         </nav>
 
-        <input
-          type="button"
-          onClick={() => setIsOpenSpNav((v) => !v)}
-          className={styles.menuButton}
-          id="menuButton"
-        />
-        <label htmlFor="menuButton" className={styles.menuIcon}>
-          <span className={styles.menuIconParts} />
-        </label>
-
-        <nav className={`${styles.spNav} ${isOpenSpNav && styles.openedSpNav}`}>
-          <ul>
-            <li className={styles.top}>
-              <Link
-                href="/posts/latest"
-                className={styles.link}
-                onClick={() => setIsOpenSpNav(false)}
-              >
-                新着記事
-              </Link>
-            </li>
-          </ul>
-        </nav>
+        <SpNav />
       </header>
-      <div className={styles.spHeaderSpacer} />
     </>
   );
 }
