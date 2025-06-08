e3ce8cbc514051404c47ad41263817c24c5d3061
takashi605 <tetocir@gmail.com>
2025-06-09 06:19:11 +0900

    OpenAPIアノテーション実装 SCRUM-292

--- source/backend/api/src/server/handlers/image_handlers.rs
+++ source/backend/api/src/server/handlers/image_handlers.rs
@@ -18,12 +18,28 @@ mod handle_funcs {
   };
   use actix_web::{web, HttpResponse, Responder};
   use common::types::api::response::Image;
+  use utoipa::path;
 
+  #[utoipa::path(
+    get,
+    path = "/api/blog/images",
+    responses(
+      (status = 200, description = "List of images", body = Vec<Image>)
+    )
+  )]
   pub async fn get_images() -> Result<impl Responder, ApiCustomError> {
     let images: Vec<Image> = fetch_images().await?;
     Ok(HttpResponse::Ok().json(images))
   }
 
+  #[utoipa::path(
+    post,
+    path = "/api/admin/blog/images",
+    request_body = Image,
+    responses(
+      (status = 200, description = "Image created", body = Image)
+    )
+  )]
   pub async fn create_image(image_by_req: web::Json<Image>) -> Result<impl Responder, ApiCustomError> {
     let image_by_req: Image = image_by_req.into_inner();
     let insert_result: ImageRecord =
