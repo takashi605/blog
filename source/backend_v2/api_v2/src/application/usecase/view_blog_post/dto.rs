c416bab287250b7b5eeccc370252939502157ee8
takashi605 <tetocir@gmail.com>
2025-06-18 13:38:13 +0900

    dto から serde への依存を削除 SCRUM-303

--- source/backend_v2/api_v2/src/application/usecase/view_blog_post/dto.rs
+++ source/backend_v2/api_v2/src/application/usecase/view_blog_post/dto.rs
@@ -1,8 +1,7 @@
 use chrono::{DateTime, NaiveDate, Utc};
-use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct ViewBlogPostDTO {
     pub id: String,
     pub title: String,
@@ -14,79 +13,64 @@ pub struct ViewBlogPostDTO {
     pub is_public: bool,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
-#[serde(rename_all = "camelCase")]
+#[derive(Debug, Clone, PartialEq)]
 pub struct ViewBlogPostImageDTO {
     pub id: Uuid,
     pub path: String,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
-#[serde(tag = "type", rename_all = "camelCase")]
+#[derive(Debug, Clone, PartialEq)]
 pub enum ViewBlogPostContentDTO {
-    #[serde(rename = "h2")]
     H2(ViewBlogPostH2BlockDTO),
-    #[serde(rename = "h3")]
     H3(ViewBlogPostH3BlockDTO),
-    #[serde(rename = "paragraph")]
     Paragraph(ViewBlogPostParagraphBlockDTO),
-    #[serde(rename = "image")]
     Image(ViewBlogPostImageBlockDTO),
-    #[serde(rename = "codeBlock")]
     Code(ViewBlogPostCodeBlockDTO),
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
-#[serde(rename_all = "camelCase")]
+#[derive(Debug, Clone, PartialEq)]
 pub struct ViewBlogPostH2BlockDTO {
     pub id: Uuid,
     pub text: String,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
-#[serde(rename_all = "camelCase")]
+#[derive(Debug, Clone, PartialEq)]
 pub struct ViewBlogPostH3BlockDTO {
     pub id: Uuid,
     pub text: String,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
-#[serde(rename_all = "camelCase")]
+#[derive(Debug, Clone, PartialEq)]
 pub struct ViewBlogPostParagraphBlockDTO {
     pub id: Uuid,
     pub text: Vec<ViewBlogPostRichTextDTO>,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
-#[serde(rename_all = "camelCase")]
+#[derive(Debug, Clone, PartialEq)]
 pub struct ViewBlogPostRichTextDTO {
     pub text: String,
     pub styles: ViewBlogPostStyleDTO,
     pub link: Option<ViewBlogPostLinkDTO>,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
-#[serde(rename_all = "camelCase")]
+#[derive(Debug, Clone, PartialEq)]
 pub struct ViewBlogPostStyleDTO {
     pub bold: bool,
     pub inline_code: bool,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
-#[serde(rename_all = "camelCase")]
+#[derive(Debug, Clone, PartialEq)]
 pub struct ViewBlogPostLinkDTO {
     pub url: String,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
-#[serde(rename_all = "camelCase")]
+#[derive(Debug, Clone, PartialEq)]
 pub struct ViewBlogPostImageBlockDTO {
     pub id: Uuid,
     pub path: String,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
-#[serde(rename_all = "camelCase")]
+#[derive(Debug, Clone, PartialEq)]
 pub struct ViewBlogPostCodeBlockDTO {
     pub id: Uuid,
     pub title: String,
--- source/backend_v2/api_v2/src/application/usecase/view_latest_blog_posts/dto.rs
+++ source/backend_v2/api_v2/src/application/usecase/view_latest_blog_posts/dto.rs
@@ -1,13 +1,12 @@
 use chrono::{DateTime, NaiveDate, Utc};
-use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct ViewLatestBlogPostsDTO {
     pub blog_posts: Vec<ViewLatestBlogPostItemDTO>,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct ViewLatestBlogPostItemDTO {
     pub id: String,
     pub title: String,
@@ -19,79 +18,64 @@ pub struct ViewLatestBlogPostItemDTO {
     pub is_public: bool,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
-#[serde(rename_all = "camelCase")]
+#[derive(Debug, Clone, PartialEq)]
 pub struct ViewLatestBlogPostImageDTO {
     pub id: Uuid,
     pub path: String,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
-#[serde(tag = "type", rename_all = "camelCase")]
+#[derive(Debug, Clone, PartialEq)]
 pub enum ViewLatestBlogPostContentDTO {
-    #[serde(rename = "h2")]
     H2(ViewLatestBlogPostH2BlockDTO),
-    #[serde(rename = "h3")]
     H3(ViewLatestBlogPostH3BlockDTO),
-    #[serde(rename = "paragraph")]
     Paragraph(ViewLatestBlogPostParagraphBlockDTO),
-    #[serde(rename = "image")]
     Image(ViewLatestBlogPostImageBlockDTO),
-    #[serde(rename = "codeBlock")]
     Code(ViewLatestBlogPostCodeBlockDTO),
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
-#[serde(rename_all = "camelCase")]
+#[derive(Debug, Clone, PartialEq)]
 pub struct ViewLatestBlogPostH2BlockDTO {
     pub id: Uuid,
     pub text: String,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
-#[serde(rename_all = "camelCase")]
+#[derive(Debug, Clone, PartialEq)]
 pub struct ViewLatestBlogPostH3BlockDTO {
     pub id: Uuid,
     pub text: String,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
-#[serde(rename_all = "camelCase")]
+#[derive(Debug, Clone, PartialEq)]
 pub struct ViewLatestBlogPostParagraphBlockDTO {
     pub id: Uuid,
     pub text: Vec<ViewLatestBlogPostRichTextDTO>,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
-#[serde(rename_all = "camelCase")]
+#[derive(Debug, Clone, PartialEq)]
 pub struct ViewLatestBlogPostRichTextDTO {
     pub text: String,
     pub styles: ViewLatestBlogPostStyleDTO,
     pub link: Option<ViewLatestBlogPostLinkDTO>,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
-#[serde(rename_all = "camelCase")]
+#[derive(Debug, Clone, PartialEq)]
 pub struct ViewLatestBlogPostStyleDTO {
     pub bold: bool,
     pub inline_code: bool,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
-#[serde(rename_all = "camelCase")]
+#[derive(Debug, Clone, PartialEq)]
 pub struct ViewLatestBlogPostLinkDTO {
     pub url: String,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
-#[serde(rename_all = "camelCase")]
+#[derive(Debug, Clone, PartialEq)]
 pub struct ViewLatestBlogPostImageBlockDTO {
     pub id: Uuid,
     pub path: String,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
-#[serde(rename_all = "camelCase")]
+#[derive(Debug, Clone, PartialEq)]
 pub struct ViewLatestBlogPostCodeBlockDTO {
     pub id: Uuid,
     pub title: String,
