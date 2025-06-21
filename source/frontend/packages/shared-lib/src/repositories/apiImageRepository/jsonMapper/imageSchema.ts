de80e7dd179bf5649109048ae37bfa36731a3acc
takashi605 <tetocir@gmail.com>
2025-06-22 08:16:02 +0900

    リポジトリを削除 SCRUM-304

--- source/frontend/packages/shared-lib/src/repositories/apiImageRepository/jsonMapper/imageSchema.ts
+++
@@ -1,7 +0,0 @@
-import type { ImageDTO } from 'service/src/imageService/dto/imageDTO';
-import { z } from 'zod';
-
-export const imageSchema: z.ZodType<ImageDTO> = z.object({
-  id: z.string(),
-  path: z.string(),
-});
