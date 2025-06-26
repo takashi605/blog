9204e05f2abfe67fa5b960b6dad2be224b8c19ac
takashi605 <tetocir@gmail.com>
2025-06-27 08:46:07 +0900

    バグ修正 インラインスタイルで pointerEvent を設定

--- source/frontend/packages/blog-admin/src/features/blogPost/create/ThumbnailPreview.tsx
+++ source/frontend/packages/blog-admin/src/features/blogPost/create/ThumbnailPreview.tsx
@@ -8,7 +8,9 @@ function ThumbnailPreview() {
   return (
     <>
       {path && (
-        <div className={styles.thumbnail}>
+        // ci 環境でサムネイル画像がクリックイベントをインターセプトする問題を回避するために pointerEvents を none に設定
+        // 手動操作では再現不可のため、根本的な解決は保留
+        <div className={styles.thumbnail} style={{ pointerEvents: 'none' }}>
           <Thumbnail path={path} />
         </div>
       )}
